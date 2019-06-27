d81d0d3c05867ab5e4bcf259262e7b2174f6d82f
Patrick Urbanke <patrick@sqlnet.ai>
2019-06-27 18:32:52 +0200

    Replaced CSRMatrix with a templated version

--- src/engine/Code/include/predictors/CSRMatrix.hpp
+++ src/engine/Code/include/predictors/CSRMatrix.hpp
@@ -5,6 +5,7 @@ namespace predictors
 {
 // -----------------------------------------------------------------------------
 
+template <typename T>
 class CSRMatrix
 {
     // -----------------------------------------------------------
@@ -35,7 +36,7 @@ class CSRMatrix
     void clear() { *this = CSRMatrix(); }
 
     /// Trivial (const) accessor.
-    const Float* data() const { return data_.data(); }
+    const T* data() const { return data_.data(); }
 
     /// Trivial (const) accessor.
     const size_t* indptr() const { return indptr_.data(); }
@@ -64,7 +65,7 @@ class CSRMatrix
 
    private:
     /// Contains the actual data.
-    std::vector<Float> data_;
+    std::vector<T> data_;
 
     /// Pointers to where columns begin and end.
     std::vector<size_t> indptr_;
@@ -77,6 +78,223 @@ class CSRMatrix
 
     // -----------------------------------------------------------
 };
+
+// -----------------------------------------------------------------------------
+}  // namespace predictors
+
+// -----------------------------------------------------------------------------
+// -----------------------------------------------------------------------------
+
+namespace predictors
+{
+// -----------------------------------------------------------------------------
+
+template <typename T>
+CSRMatrix<T>::CSRMatrix( const CFloatColumn& _col )
+{
+    assert( _col );
+
+    data_ = *_col;
+
+    indices_ = std::vector<size_t>( _col->size() );
+
+    indptr_ = std::vector<size_t>( _col->size() + 1 );
+
+    for ( size_t i = 0; i < indptr_.size(); ++i )
+        {
+            indptr_[i] = i;
+        }
+
+    ncols_ = 1;
+}
+
+// -----------------------------------------------------------------------------
+
+template <typename T>
+CSRMatrix<T>::CSRMatrix( const CIntColumn& _col, const size_t _n_unique )
+{
+    assert( _col );
+
+    indptr_ = std::vector<size_t>( _col->size() + 1 );
+
+    for ( size_t i = 0; i < _col->size(); ++i )
+        {
+            if ( ( *_col )[i] >= 0 )
+                {
+                    indices_.push_back( static_cast<size_t>( ( *_col )[i] ) );
+
+                    indptr_[i + 1] = indptr_[i] + 1;
+                }
+            else
+                {
+                    indptr_[i + 1] = indptr_[i];
+                }
+        }
+
+    data_ = std::vector<T>( indices_.size(), 1.0 );
+
+    ncols_ += _n_unique;
+}
+
+// -----------------------------------------------------------------------------
+
+template <typename T>
+void CSRMatrix<T>::add( const CFloatColumn& _col )
+{
+    // -------------------------------------------------------------------------
+
+    assert( _col );
+
+    // -------------------------------------------------------------------------
+    // If the CSRMatrix is empty, simply construct it from the column.
+
+    if ( ncols() == 0 )
+        {
+            *this = CSRMatrix( _col );
+            return;
+        }
+
+    assert( _col->size() == nrows() );
+
+    // -------------------------------------------------------------------------
+    // Adapt data_.
+
+    auto data_temp = std::vector<T>( data_.size() + _col->size() );
+
+    for ( size_t i = 0; i < nrows(); ++i )
+        {
+            std::copy(
+                data_.begin() + indptr_[i],
+                data_.begin() + indptr_[i + 1],
+                data_temp.begin() + indptr_[i] + i );
+
+            data_temp[indptr_[i + 1] + i] = static_cast<T>( ( *_col )[i] );
+        }
+
+    data_ = std::move( data_temp );
+
+    // -------------------------------------------------------------------------
+    // Adapt indices_.
+
+    auto indices_temp = std::vector<size_t>( indices_.size() + _col->size() );
+
+    for ( size_t i = 0; i < nrows(); ++i )
+        {
+            std::copy(
+                indices_.begin() + indptr_[i],
+                indices_.begin() + indptr_[i + 1],
+                indices_temp.begin() + indptr_[i] + i );
+
+            indices_temp[indptr_[i + 1] + i] = ncols_;
+        }
+
+    indices_ = std::move( indices_temp );
+
+    // -------------------------------------------------------------------------
+    // Adapt indptr_.
+
+    std::for_each(
+        indptr_.begin() + 1, indptr_.end(), []( size_t& _val ) { _val += 1; } );
+
+    // -------------------------------------------------------------------------
+    // Finally, we must increment ncols_.
+
+    ++ncols_;
+
+    // -------------------------------------------------------------------------
+}
+
+// -----------------------------------------------------------------------------
+
+template <typename T>
+void CSRMatrix<T>::add( const CIntColumn& _col, const size_t _n_unique )
+{
+    // -------------------------------------------------------------------------
+
+    assert( _col );
+
+    // -------------------------------------------------------------------------
+    // If the CSRMatrix is empty, simply construct it from the column.
+
+    if ( ncols() == 0 )
+        {
+            *this = CSRMatrix( _col, _n_unique );
+            return;
+        }
+
+    assert( _col->size() == nrows() );
+
+    // -------------------------------------------------------------------------
+    // Count the number of non-negative entries in _col.
+
+    auto is_non_negative = []( int val ) { return ( val >= 0 ); };
+
+    const size_t num_non_negative =
+        std::count_if( _col->begin(), _col->end(), is_non_negative );
+
+    // -------------------------------------------------------------------------
+    // Adapt data_ and indices_
+
+    auto data_temp = std::vector<T>( data_.size() + num_non_negative );
+
+    auto indices_temp =
+        std::vector<size_t>( indices_.size() + num_non_negative );
+
+    size_t num_added = 0;
+
+    for ( size_t i = 0; i < nrows(); ++i )
+        {
+            std::copy(
+                data_.begin() + indptr_[i],
+                data_.begin() + indptr_[i + 1],
+                data_temp.begin() + indptr_[i] + num_added );
+
+            std::copy(
+                indices_.begin() + indptr_[i],
+                indices_.begin() + indptr_[i + 1],
+                indices_temp.begin() + indptr_[i] + num_added );
+
+            if ( ( *_col )[i] >= 0 )
+                {
+                    data_temp[indptr_[i + 1] + num_added] = 1.0;
+
+                    indices_temp[indptr_[i + 1] + num_added] =
+                        static_cast<size_t>( ( *_col )[i] ) + ncols();
+
+                    ++num_added;
+                }
+        }
+
+    assert( num_added == num_non_negative );
+
+    data_ = std::move( data_temp );
+
+    indices_ = std::move( indices_temp );
+
+    // -------------------------------------------------------------------------
+    // Adapt indptr_.
+
+    num_added = 0;
+
+    for ( size_t i = 0; i < _col->size(); ++i )
+        {
+            if ( ( *_col )[i] >= 0 )
+                {
+                    ++num_added;
+                }
+
+            indptr_[i + 1] += num_added;
+        }
+
+    assert( num_added == num_non_negative );
+
+    // -------------------------------------------------------------------------
+    // Finally, we must adapt ncols_.
+
+    ncols_ += _n_unique;
+
+    // -------------------------------------------------------------------------
+}
 
 // -----------------------------------------------------------------------------
 }  // namespace predictors
