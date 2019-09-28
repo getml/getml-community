71a952ea8b38ee5b24f641a01aff81bc4fe2e7cb
Patrick Urbanke <patrick@sqlnet.ai>
2019-09-28 12:27:44 +0200

    Added MultirelModelManager

---
+++ src/engine/Code/include/engine/handlers/MultirelModelManager.hpp
@@ -0,0 +1,15 @@
+#ifndef ENGINE_HANDLERS_MULTIRELMODELMANAGER_HPP_
+#define ENGINE_HANDLERS_MULTIRELMODELMANAGER_HPP_
+
+namespace engine
+{
+namespace handlers
+{
+typedef ModelManager<models::MultirelModel> MultirelModelManager;
+
+}  // namespace handlers
+}  // namespace engine
+
+#endif  // ENGINE_HANDLERS_MULTIRELMODELMANAGER_HPP_
+
+// ------------------------------------------------------------------------
