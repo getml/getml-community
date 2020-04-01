86e0682fee834f052fe81378d8b8c2a2137bb82a
Patrick Urbanke <patrick@sqlnet.ai>
2020-04-01 14:51:32 +0200

    Adde PredTracker

---
+++ src/engine/Code/include/engine/dependency/PredTracker.hpp
@@ -0,0 +1,16 @@
+#ifndef ENGINE_DEPENDENCY_PREDTRACKER_HPP_
+#define ENGINE_DEPENDENCY_PREDTRACKER_HPP_
+
+namespace engine
+{
+namespace dependency
+{
+// -------------------------------------------------------------------------
+
+typedef Tracker<predictors::Predictor> PredTracker;
+
+// -------------------------------------------------------------------------
+}  // namespace dependency
+}  // namespace engine
+
+#endif  // ENGINE_DEPENDENCY_PREDTRACKER_HPP_
