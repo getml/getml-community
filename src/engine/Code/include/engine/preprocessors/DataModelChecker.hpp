a9fe036dd96fdcac248d85cf8a0e7bde05a4067e
Patrick Urbanke <patrick@sqlnet.ai>
2021-01-03 18:11:18 +0100

    DataModelChecker checks for binary columns

--- src/engine/Code/include/engine/preprocessors/DataModelChecker.hpp
+++ src/engine/Code/include/engine/preprocessors/DataModelChecker.hpp
@@ -152,6 +152,12 @@ class DataModelChecker
         const std::string& _df_name,
         communication::Warner* _warner );
 
+    /// Adds a warning for categorical columns with only two unique values.
+    static void warn_binary(
+        const std::string& _colname,
+        const std::string& _df_name,
+        communication::Warner* _warner );
+
     /// Adds a warning that a data frame is empty.
     static void warn_is_empty(
         const std::string& _df_name, communication::Warner* _warner );
@@ -240,6 +246,9 @@ class DataModelChecker
     // -------------------------------------------------------------------------
 
    private:
+    /// Standard header for binary column.
+    static std::string binary_column() { return info() + "[BINARY COLUMN]: "; }
+
     /// Standard header for a column that should be unused.
     static std::string column_should_be_unused()
     {
