7cc7cc61076800a43efb88cd934d67c45819b31e
Sören Nikolaus <soeren.nikolaus@getml.com>
2021-04-07 13:48:27 +0200

    Fixes and adjustments to parser

--- src/engine/Code/include/io/Parser.hpp
+++ src/engine/Code/include/io/Parser.hpp
@@ -10,20 +10,6 @@ class Parser
    public:
     // -------------------------------
 
-    static std::string format_fixed_and_trim_zeros(
-        const Float val, const unsigned long precision )
-    {
-        std::ostringstream stream;
-
-        stream << std::fixed << std::setprecision( precision ) << val;
-        const auto str = stream.str();
-        const auto pos = str.find_last_not_of( "0" ) + 1;
-
-        return str.substr( 0, pos );
-    }
-
-    // -------------------------------
-
     /// Removes the quotechars.
     static std::string remove_quotechars(
         const std::string& _str, const char _quotechar )
@@ -136,11 +122,7 @@ class Parser
     static std::string to_string( const Float _val )
     {
         constexpr unsigned long precision = 4;
-        constexpr Float delta = 1 / std::pow( 10, precision );
-
-        const auto is_full = []( const Float val ) {
-            return std::floor( val ) == val;
-        };
+        constexpr Float delta =  1.0 / std::pow(10.0, static_cast<Float>(precision) );
 
         const auto is_approx_full = [ precision, delta ]( const Float val ) {
             return std::fmod( val, 1.0 ) <= delta;
@@ -148,13 +130,13 @@ class Parser
 
         std::ostringstream stream;
 
-        if ( is_full( _val ) || is_approx_full( _val ) )
+        if ( is_approx_full( _val ) )
             {
                 stream << static_cast<long>( _val );
             }
         else if ( _val > 1.0 ) 
             {
-                stream << format_fixed_and_trim_zeros( _val, precision );
+                stream << format_fixed_no_trailing_zeros( _val, precision );
             }
         else 
             {
@@ -248,6 +230,26 @@ class Parser
     }
 
     // -------------------------------
+
+   private:
+
+    // -------------------------------
+
+    static std::string format_fixed_no_trailing_zeros(
+        const Float _val, const unsigned long _precision )
+    {
+        std::ostringstream stream;
+
+        stream << std::fixed << std::setprecision( _precision ) << _val;
+        const auto str = stream.str();
+        const auto pos = str.find_last_not_of( "0" ) + 1;
+
+        return str.substr( 0, pos );
+
+    // -------------------------------
+
+    }
+
 };
 
 // ----------------------------------------------------------------------------
