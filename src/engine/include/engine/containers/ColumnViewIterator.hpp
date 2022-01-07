5a30400e15db730e99f61437245c8f294a282d31
Patrick Urbanke <patrick@getml.com>
2022-01-07 01:17:14 +0100

    Designed the ColumnViewIterator as an input iterator

--- src/engine/include/engine/containers/ColumnViewIterator.hpp
+++ src/engine/include/engine/containers/ColumnViewIterator.hpp
@@ -3,9 +3,11 @@
 
 // -------------------------------------------------------------------------
 
+#include <functional>
 #include <iterator>
 #include <memory>
 #include <optional>
+#include <utility>
 
 // -------------------------------------------------------------------------
 
@@ -15,12 +17,11 @@
 
 namespace engine {
 namespace containers {
-// -------------------------------------------------------------------------
 
 template <class T>
 class ColumnViewIterator {
  public:
-  using iterator_category = std::random_access_iterator_tag;
+  using iterator_category = std::input_iterator_tag;
   using difference_type = std::ptrdiff_t;
   using value_type = T;
   using pointer = value_type*;
@@ -30,35 +31,18 @@ class ColumnViewIterator {
 
   /// Iterator to the beginning.
   explicit ColumnViewIterator(const ValueFunc _value_func)
-      : i_(0),
-        subiterator_(std::make_shared<ColumnViewIterator<T>>()),
-        value_(_value_func(0)),
-        value_func_(_value_func),
-        value_ptr_(&value_){};
+      : i_(0), value_func_(_value_func) {}
 
   /// Iterator to the end.
-  ColumnViewIterator()
-      : i_(0),
-        subiterator_(nullptr),
-        value_(std::nullopt),
-        value_func_(std::nullopt),
-        value_ptr_(&value_){};
+  ColumnViewIterator() : i_(0), value_func_(std::nullopt) {}
 
   /// Copy constructor.
   ColumnViewIterator(const ColumnViewIterator<T>& _other)
-      : i_(_other.i_),
-        subiterator_(std::make_shared<ColumnViewIterator<T>>()),
-        value_(_other.value_),
-        value_func_(_other.value_func_),
-        value_ptr_(&value_){};
+      : i_(_other.i_), value_func_(_other.value_func_) {}
 
   /// Move constructor.
   ColumnViewIterator(ColumnViewIterator<T>&& _other) noexcept
-      : i_(_other.i_),
-        subiterator_(std::move(_other.subiterator_)),
-        value_(std::move(_other.value_)),
-        value_func_(std::move(_other.value_func_)),
-        value_ptr_(&value_){};
+      : i_(_other.i_), value_func_(std::move(_other.value_func_)) {}
 
   /// Destructor.
   ~ColumnViewIterator() = default;
@@ -66,8 +50,6 @@ class ColumnViewIterator {
   /// Copy assignment operator.
   ColumnViewIterator<T>& operator=(const ColumnViewIterator<T>& _other) {
     i_ = _other.i_;
-    subiterator_ = _other.subiterator_;
-    value_ = _other.value_;
     value_func_ = _other.value_func_;
     return *this;
   }
@@ -75,32 +57,23 @@ class ColumnViewIterator {
   /// Move assignment operator.
   ColumnViewIterator<T>& operator=(ColumnViewIterator<T>&& _other) noexcept {
     i_ = _other.i_;
-    subiterator_ = std::move(_other.subiterator_);
-    value_ = std::move(_other.value_);
     value_func_ = std::move(_other.value_func_);
     return *this;
   }
 
-  /// Tricky: operator()* must be const, but provide a non-const reference.
-  inline reference operator*() const {
-    assert_true(value_);
-    assert_true(*value_ptr_);
-    return **value_ptr_;
+  /// Deference operator
+  inline value_type operator*() const {
+    assert_true(value_func_);
+    assert_true((*value_func_)(i_));
+    return *(*value_func_)(i_);
   }
 
-  /// Returns a pointer to value_.
-  inline pointer operator->() {
-    assert_true(value_);
-    assert_true(*value_ptr_);
-    return &(**value_ptr_);
-  }
+  /// Deference operator
+  inline value_type operator->() const { return **this; }
 
   /// Prefix incrementor
   inline ColumnViewIterator<T>& operator++() {
     ++i_;
-    if (value_func_) {
-      value_ = (*value_func_)(i_);
-    }
     return *this;
   }
 
@@ -111,64 +84,13 @@ class ColumnViewIterator {
     return tmp;
   }
 
-  /// Incrementor
-  inline ColumnViewIterator<T>& operator+=(difference_type _j) {
-    const auto i = static_cast<difference_type>(i_) + _j;
-    assert_true(i >= 0);
-    i_ = static_cast<size_t>(i);
-    if (value_func_) {
-      value_ = (*value_func_)(i_);
-    }
-    return *this;
-  }
-
-  /// Prefix decrementor
-  inline ColumnViewIterator<T>& operator--() {
-    --i_;
-    if (value_func_) {
-      value_ = (*value_func_)(i_);
-    }
-    return *this;
-  }
-
-  /// Postfix decrementor.
-  inline ColumnViewIterator<T> operator--(int) {
-    ColumnViewIterator<T> tmp = *this;
-    --(*this);
-    return tmp;
-  }
-
-  /// Decrementor
-  inline ColumnViewIterator<T>& operator-=(size_t _j) {
-    const auto i = static_cast<difference_type>(i_) - _j;
-    assert_true(i >= 0);
-    i_ = static_cast<size_t>(i);
-    if (value_func_) {
-      value_ = (*value_func_)(i_);
-    }
-    return *this;
-  }
-
-  /// operator[], needed for ranges support.
-  inline reference operator[](difference_type _i) const {
-    assert_true(subiterator_);
-    *subiterator_ = *this + _i;
-    return **subiterator_;
-  }
-
   /// Check equality
   friend inline bool operator==(const ColumnViewIterator<T>& _a,
                                 const ColumnViewIterator<T>& _b) {
-    return (!_a.value_ && !_b.value_) ||
-           (_a.value_ && _b.value_ && _a.i_ == _b.i_);
-  };
-
-  /// Smaller than operator.
-  friend inline bool operator<(const ColumnViewIterator<T>& _a,
-                               const ColumnViewIterator<T>& _b) {
-    return (_a.value_ && !_b.value_) || !(!_a.value_ && _b.value_) ||
-           (_a.value_ && _b.value_ && _a.i_ < _b.i_);
-  };
+    const auto opt_a = _a.value_func_ ? (*_a.value_func_)(_a.i_) : std::nullopt;
+    const auto opt_b = _b.value_func_ ? (*_b.value_func_)(_b.i_) : std::nullopt;
+    return (!opt_a && !opt_b) || (opt_a && opt_b && _a.i_ == _b.i_);
+  }
 
   /// Inequality operator.
   friend inline bool operator!=(const ColumnViewIterator<T>& _a,
@@ -176,68 +98,12 @@ class ColumnViewIterator {
     return !(_a == _b);
   }
 
-  /// Greater than opeator
-  friend inline bool operator>(const ColumnViewIterator<T>& _a,
-                               const ColumnViewIterator<T>& _b) {
-    return _b < _a;
-  }
-
-  /// Smaller-equal operator.
-  friend inline bool operator<=(const ColumnViewIterator<T>& _a,
-                                const ColumnViewIterator<T>& _b) {
-    return !(_b < _a);
-  }
-
-  friend inline bool operator>=(const ColumnViewIterator<T>& _a,
-                                const ColumnViewIterator<T>& _b) {
-    return !(_a < _b);
-  }
-
-  /// Add value
-  friend inline ColumnViewIterator<T> operator+(const ColumnViewIterator<T>& _a,
-                                                const difference_type _j) {
-    ColumnViewIterator<T> tmp = _a;
-    tmp += _j;
-    return tmp;
-  }
-
-  /// Add value
-  friend inline ColumnViewIterator<T> operator+(
-      const difference_type _j, const ColumnViewIterator<T>& _a) {
-    return _a + _j;
-  }
-
-  /// Substract value
-  friend inline ColumnViewIterator<T> operator-(const ColumnViewIterator<T>& _a,
-                                                const difference_type _j) {
-    ColumnViewIterator<T> tmp = _a;
-    tmp -= _j;
-    return tmp;
-  }
-
-  /// Get difference between two iterators
-  friend inline difference_type operator-(const ColumnViewIterator<T>& _a,
-                                          const ColumnViewIterator<T>& _b) {
-    const auto i_a = static_cast<difference_type>(_a.i_);
-    const auto i_b = static_cast<difference_type>(_b.i_);
-    return i_a - i_b;
-  }
-
  private:
   /// The current index.
   size_t i_;
 
-  /// The subiterator is needed for the operator[].
-  std::shared_ptr<ColumnViewIterator<T>> subiterator_;
-
-  /// The current value.
-  std::optional<T> value_;
-
   /// The function returning the actual data point.
   std::optional<ValueFunc> value_func_;
-
-  /// A pointer to the value_;
-  std::optional<T>* const value_ptr_;
 };
 
 // -------------------------------------------------------------------------
