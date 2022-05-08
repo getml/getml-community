6bcbf7dbf00d00f26a6ddbdd80a88430a139ca02
Patrick Urbanke <patrick@getml.com>
2022-05-08 14:49:00 +0200

    Removed all transpilation components other than HumanReadableSQL

--- src/engine/src/transpilation/SQLDialectParser.cpp
+++ src/engine/src/transpilation/SQLDialectParser.cpp
@@ -1,43 +1,22 @@
 #include "transpilation/SQLDialectParser.hpp"
 
-#include "transpilation/BigQueryGenerator.hpp"
 #include "transpilation/HumanReadableSQLGenerator.hpp"
-#include "transpilation/MySQLGenerator.hpp"
-#include "transpilation/PostgreSQLGenerator.hpp"
-#include "transpilation/SparkSQLGenerator.hpp"
-#include "transpilation/TSQLGenerator.hpp"
 #include "transpilation/TranspilationParams.hpp"
 
 namespace transpilation {
 
 fct::Ref<const SQLDialectGenerator> SQLDialectParser::parse(
     const TranspilationParams& _params) {
-  if (_params.dialect_ == BIG_QUERY) {
-    return fct::Ref<const BigQueryGenerator>::make(_params);
-  }
-
   if (_params.dialect_ == HUMAN_READABLE_SQL) {
     return fct::Ref<const HumanReadableSQLGenerator>::make();
   }
 
-  if (_params.dialect_ == MYSQL) {
-    return fct::Ref<const MySQLGenerator>::make(_params);
-  }
-
-  if (_params.dialect_ == POSTGRE_SQL) {
-    return fct::Ref<const PostgreSQLGenerator>::make(_params);
-  }
-
-  if (_params.dialect_ == SPARK_SQL) {
-    return fct::Ref<const SparkSQLGenerator>::make();
-  }
-
-  if (_params.dialect_ == SQLITE3) {
-    return fct::Ref<const HumanReadableSQLGenerator>::make();
-  }
-
-  if (_params.dialect_ == TSQL) {
-    return fct::Ref<const TSQLGenerator>::make(_params);
+  if (_params.dialect_ == BIG_QUERY || _params.dialect_ == MYSQL ||
+      _params.dialect_ == POSTGRE_SQL || _params.dialect_ == SPARK_SQL ||
+      _params.dialect_ == SQLITE3 || _params.dialect_ == TSQL) {
+    throw std::runtime_error(
+        "Transpiling to '" + _params.dialect_ +
+        "' is not supported in the getML community edition.");
   }
 
   throw std::runtime_error("Unknown SQL dialect: '" + _params.dialect_ + "'.");
