e3c5758b2db638b7e98a4e96981e3d1a99325b97
Patrick Urbanke <patrick@getml.com>
2022-10-18 06:38:47 +0200

    Added support for installing into /usr/local/; #288

--- src/getml-app/src/install/All.go
+++ src/getml-app/src/install/All.go
@@ -1,9 +1,9 @@
 // Copyright 2022 The SQLNet Company GmbH
-// 
-// This file is licensed under the Elastic License 2.0 (ELv2). 
-// Refer to the LICENSE.txt file in the root of the repository 
+//
+// This file is licensed under the Elastic License 2.0 (ELv2).
+// Refer to the LICENSE.txt file in the root of the repository
 // for details.
-// 
+//
 
 package install
 
@@ -23,8 +23,20 @@ func All(homeDir string, version string) {
 	}
 
 	if runtime.GOOS == "linux" {
-		copyResources(homeDir, version)
-		MakeDesktopEntry(homeDir, version)
+		err := copyResources(UsrLocal, version)
+
+		if err != nil {
+			println("Could not install into '" + UsrLocal + "': " + err.Error())
+		} else {
+			copyFile("getML", "", UsrLocalBin, true)
+			return
+		}
+
+		err = copyResources(homeDir, version)
+
+		if err != nil {
+			println("Could not install into '" + homeDir + "': " + err.Error())
+		}
 	}
 
 }
--- src/getml-app/src/install/GetMainDir.go
+++ src/getml-app/src/install/GetMainDir.go
@@ -1,9 +1,9 @@
 // Copyright 2022 The SQLNet Company GmbH
-// 
-// This file is licensed under the Elastic License 2.0 (ELv2). 
-// Refer to the LICENSE.txt file in the root of the repository 
+//
+// This file is licensed under the Elastic License 2.0 (ELv2).
+// Refer to the LICENSE.txt file in the root of the repository
 // for details.
-// 
+//
 
 package install
 
@@ -14,12 +14,16 @@ import (
 
 // GetMainDir returns the main directory
 // to which we are copying our files.
-func GetMainDir(homeDir string, version string) string {
+func GetMainDir(targetDir string, version string) string {
 
 	if runtime.GOOS == "windows" {
 		return "."
 	}
 
-	return filepath.Join(GetHomeDir(homeDir), ".getML/"+version)
+	if targetDir == UsrLocal {
+		return filepath.Join(GetHomeDir(targetDir), "getML/"+version)
+	}
+
+	return filepath.Join(GetHomeDir(targetDir), ".getML/"+version)
 
 }
