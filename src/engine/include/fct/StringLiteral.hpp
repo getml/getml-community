8be945e10dec5d572cc0b5e18eff59fbc4525842
Patrick Urbanke <patrick@getml.com>
2022-11-30 07:19:41 +0100

    Inline the equality operators on StringLiteral

--- src/engine/include/fct/StringLiteral.hpp
+++ src/engine/include/fct/StringLiteral.hpp
@@ -23,25 +23,25 @@ struct StringLiteral {
 
   char value_[N];
 
-  template <size_t N1, size_t N2>
-  friend constexpr bool operator==(const StringLiteral<N1>& _first,
-                                   const StringLiteral<N2>& _second) {
-    if constexpr (N1 != N2) {
-      return false;
-    }
-    return std::string_view(_first.value_, N1) ==
-           std::string_view(_second.value_, N2);
-  }
-
-  template <size_t N1, size_t N2>
-  friend constexpr bool operator!=(const StringLiteral<N1>& _first,
-                                   const StringLiteral<N2>& _second) {
-    return !(_first == _second);
-  }
-
   /// Returns the value as a string.
   std::string str() const { return std::string(value_); }
 };
+
+template <size_t N1, size_t N2>
+constexpr inline bool operator==(const StringLiteral<N1>& _first,
+                                 const StringLiteral<N2>& _second) {
+  if constexpr (N1 != N2) {
+    return false;
+  }
+  return std::string_view(_first.value_, N1) ==
+         std::string_view(_second.value_, N2);
+}
+
+template <size_t N1, size_t N2>
+constexpr inline bool operator!=(const StringLiteral<N1>& _first,
+                                 const StringLiteral<N2>& _second) {
+  return !(_first == _second);
+}
 
 }  // namespace fct
 
