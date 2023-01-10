e6b102bfe4942db5e816a507e6302cea74e195e4
Patrick Urbanke <patrick@getml.com>
2023-01-10 10:32:46 +0100

    added a visitor pattern to the Literal

--- src/engine/include/fct/Literal.hpp
+++ src/engine/include/fct/Literal.hpp
@@ -39,18 +39,44 @@ struct GetValueOf {
   }
 };
 
+/// Finds the correct index associated with
+/// the string.
+template <int _i, int total, StringLiteral _head, StringLiteral... _tail>
+inline constexpr auto find_name_of() {
+  constexpr int n = total - sizeof...(_tail) - 1;
+  if constexpr (n == _i) {
+    return _head;
+  } else if (n + 1 < total) {
+    return find_name_of<_i, total, _tail...>();
+  }
+}
+
 template <StringLiteral... fields_>
 class Literal {
  public:
   using ValueType =
-      typename std::conditional<sizeof...(fields_) <=
-                                    std::numeric_limits<std::uint8_t>::max(),
-                                std::uint8_t, std::uint16_t>::type;
+      std::conditional_t<sizeof...(fields_) <=
+                             std::numeric_limits<std::uint8_t>::max(),
+                         std::uint8_t, std::uint16_t>;
+
+  /// The number of different fields or different options that the literal
+  /// can assume.
+  static constexpr ValueType num_fields_ = sizeof...(fields_);
 
   /// Constructs a Literal from a string.
   Literal(const std::string& _str) : value_(find_value<fields_...>(_str)) {
+    static_assert(sizeof...(fields_) > 0,
+                  "There must be at least one field in a Literal.");
     static_assert(sizeof...(fields_) <= std::numeric_limits<ValueType>::max(),
                   "Too many fields.");
+  }
+
+  /// A single-field literal is special because it
+  /// can also have a default constructor.
+  Literal() : value_(0) {
+    static_assert(
+        num_fields_ == 1,
+        "Only Literals with a single field can use the default constructor.");
   }
 
   ~Literal() = default;
@@ -64,6 +90,14 @@ class Literal {
   /// The name defined by the Literal.
   std::string name() const { return find_name<0, fields_...>(); }
 
+  /// Helper function to retrieve a name at compile time.
+  template <int _value>
+  inline constexpr static auto name_of() {
+    constexpr auto name =
+        find_name_of<_value, sizeof...(fields_), fields_...>();
+    return Literal<name>();
+  }
+
   /// Assigns the literal from a string
   Literal operator=(const std::string& _str) {
     value_ = find_value<fields_...>(_str);
@@ -75,9 +109,16 @@ class Literal {
     return value() == _other.value();
   }
 
+  /// Equality operator other Literals with different fields.
+  template <StringLiteral... other_fields>
+  inline bool operator==(const Literal<other_fields...>& _other) const {
+    return name() == _other.name();
+  }
+
   /// Inequality operator for other Literals.
-  inline bool operator!=(const Literal<fields_...>& _other) const {
-    return value() != _other.value();
+  template <StringLiteral... other_fields>
+  inline bool operator!=(const Literal<other_fields...>& _other) const {
+    return !(*this == _other);
   }
 
   /// Equality operator for strings.
@@ -141,6 +182,24 @@ class Literal {
   ValueType value_;
 };
 
+/// Helper class to retrieve a name from a literal.
+template <class LiteralType, int value_>
+struct GetNameOfLiteral;
+
+/// Helper class to retrieve a name from a literal.
+template <StringLiteral... fields_, int value_>
+struct GetNameOfLiteral<Literal<fields_...>, value_> {
+  static constexpr auto name_ =
+      find_name_of<value_, sizeof...(fields_), fields_...>();
+};
+
+/// Helper function to retrieve a name at compile time.
+template <class LiteralType, int _value>
+inline constexpr auto name_of() {
+  constexpr auto name = GetNameOfLiteral<LiteralType, _value>::name_;
+  return Literal<name>();
+}
+
 /// Helper class to retrieve a value from a literal.
 template <class LiteralType, StringLiteral name_>
 struct GetValueOfLiteral;
@@ -155,6 +214,18 @@ struct GetValueOfLiteral<Literal<fields_...>, name_> {
 template <class LiteralType, StringLiteral _name>
 inline constexpr auto value_of() {
   return GetValueOfLiteral<LiteralType, _name>::get();
+}
+
+/// Implements the visitor pattern for Literals.
+template <class Visitor, int i = 0, StringLiteral... _fields>
+inline auto visit(const Visitor& _visitor, const Literal<_fields...> _literal) {
+  constexpr typename Literal<_fields...>::ValueType value = i;
+  if (_literal.value() == value) {
+    return _visitor(name_of<Literal<_fields...>, i>());
+  }
+  if constexpr (i + 1 < sizeof...(_fields)) {
+    return visit<Visitor, i + 1, _fields...>(_visitor, _literal);
+  }
 }
 
 }  // namespace fct
