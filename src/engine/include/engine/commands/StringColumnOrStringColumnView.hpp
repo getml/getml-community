ea3edbed2ed718fc80d3faa80f4f79cd020acfe1
Patrick Urbanke <patrick@getml.com>
2023-01-12 20:40:01 +0100

    Largely rewrote the CatOpParser

--- src/engine/include/engine/commands/StringColumnOrStringColumnView.hpp
+++ src/engine/include/engine/commands/StringColumnOrStringColumnView.hpp
@@ -8,17 +8,86 @@
 #ifndef ENGINE_COMMANDS_STRINGCOLUMNORSTRINGCOLUMNVIEW_HPP_
 #define ENGINE_COMMANDS_STRINGCOLUMNORSTRINGCOLUMNVIEW_HPP_
 
+#include <string>
 #include <variant>
 
-#include "engine/commands/StringColumn.hpp"
-#include "engine/commands/StringColumnView.hpp"
+#include "fct/Field.hpp"
+#include "fct/Literal.hpp"
+#include "fct/NamedTuple.hpp"
+#include "fct/Ref.hpp"
 
 namespace engine {
 namespace commands {
 
 class StringColumnOrStringColumnView {
  public:
-  using RecursiveType = std::variant<StringColumn, StringColumnView>;
+  /// The possible operators for a binary operations.
+  using StringBinaryOpLiteral = fct::Literal<"concat">;
+
+  /// The command used for boolean binary operations.
+  using StringBinaryOp = fct::NamedTuple<
+      fct::Field<"operand1_", fct::Ref<StringColumnOrStringColumnView>>,
+      fct::Field<"operand2_", fct::Ref<StringColumnOrStringColumnView>>,
+      fct::Field<"operator_", StringBinaryOpLiteral>,
+      fct::Field<"type_", fct::Literal<"StringColumnView">>>;
+
+  /// The command used for string const operations.
+  using StringConstOp =
+      fct::NamedTuple<fct::Field<"value_", std::string>,
+                      fct::Field<"operator_", fct::Literal<"const">>,
+                      fct::Field<"type_", fct::Literal<"StringColumnView">>>;
+
+  /// The command used to retrieve a substring.
+  using StringSubstringOp = fct::NamedTuple<
+      fct::Field<"begin_", size_t>, fct::Field<"len_", size_t>,
+      fct::Field<"operand1_", fct::Ref<StringColumnOrStringColumnView>>,
+      fct::Field<"operator_", fct::Literal<"substr">>,
+      fct::Field<"type_", fct::Literal<"StringColumnView">>>;
+
+  /// The possible operators.
+  using StringUnaryOpLiteral = fct::Literal<"as_str">;
+
+  /// The command used for string unary operations.
+  using StringUnaryOp = fct::NamedTuple<
+      fct::Field<"operand1_", fct::Ref<StringColumnOrStringColumnView>>,
+      fct::Field<"operator_", StringUnaryOpLiteral>,
+      fct::Field<"type_", fct::Literal<"StringColumnView">>>;
+
+  /// The command used for string with subtoles operations.
+  using StringWithSubrolesOp = fct::NamedTuple<
+      fct::Field<"operand1_", fct::Ref<StringColumnOrStringColumnView>>,
+      fct::Field<"subroles_", std::vector<std::string>>,
+      fct::Field<"type_", fct::Literal<"StringColumnView">>>;
+
+  /// The command used for string with unit operations.
+  using StringWithUnitOp = fct::NamedTuple<
+      fct::Field<"operand1_", fct::Ref<StringColumnOrStringColumnView>>,
+      fct::Field<"unit_", std::string>,
+      fct::Field<"type_", fct::Literal<"StringColumnView">>>;
+
+  /// The command used for boolean subselection operations.
+  // TODO
+  /*using StringSubselectionOp =
+      fct::NamedTuple<fct::Field<"operand1_", Column>,
+                      fct::Field<"operand2_", Column>,
+                      fct::Field<"operator_", fct::Literal<"subselection">>,
+                      fct::Field<"type_", fct::Literal<"StringColumnView">>>;*/
+
+  /// The command used for retrieving string columns from a data frame.
+  using StringColumnOp =
+      fct::NamedTuple<fct::Field<"df_name_", std::string>,
+                      fct::Field<"name_", std::string>,
+                      fct::Field<"type_", fct::Literal<"StringColumn">>>;
+
+  /// Defines a string column view.
+  using StringColumnView =
+      std::variant<StringBinaryOp, StringConstOp, /*StringSubselectionOp,*/
+                   StringUnaryOp, StringWithSubrolesOp, StringWithUnitOp>;
+
+  using RecursiveType = std::variant<StringColumnOp, StringBinaryOp,
+                                     StringConstOp, /*StringSubselectionOp,*/
+                                     /*StringUnaryOp,*/ StringSubstringOp,
+                                     StringWithSubrolesOp, StringWithUnitOp>;
 
   /// Used to break the recursive definition.
   RecursiveType val_;
--- src/engine/src/engine/CatOpParser.cpp
+++ src/engine/src/engine/CatOpParser.cpp
@@ -14,28 +14,16 @@ namespace engine {
 namespace handlers {
 
 containers::ColumnView<strings::String> CatOpParser::binary_operation(
-    const Poco::JSON::Object& _col) const {
-  const auto op = JSON::get_value<std::string>(_col, "operator_");
+    const StringBinaryOp& _cmd) const {
+  const auto concat = [](const strings::String& _val1,
+                         const strings::String& _val2) -> strings::String {
+    if (!_val1 || !_val2) {
+      return strings::String(nullptr);
+    }
+    return strings::String(_val1.str() + _val2.str());
+  };
 
-  if (op == "concat") {
-    const auto concat = [](const strings::String& _val1,
-                           const strings::String& _val2) -> strings::String {
-      if (!_val1 || !_val2) {
-        return strings::String(nullptr);
-      }
-      return strings::String(_val1.str() + _val2.str());
-    };
-    return bin_op(_col, concat);
-  }
-
-  if (op == "update") {
-    return update(_col);
-  }
-
-  throw std::runtime_error("Operator '" + op +
-                           "' not recognized for categorical columns.");
-
-  return update(_col);
+  return bin_op(_cmd, concat);
 }
 
 // ----------------------------------------------------------------------------
@@ -91,6 +79,47 @@ void CatOpParser::check(const containers::Column<strings::String>& _col,
 
 // ----------------------------------------------------------------------------
 
+containers::ColumnView<strings::String> CatOpParser::get_column(
+    const StringColumnOp& _cmd) const {
+  const auto name = _cmd.get<"name_">();
+
+  const auto df_name = _cmd.get<"df_name_">();
+
+  const auto it = data_frames_->find(df_name);
+
+  if (it == data_frames_->end()) {
+    throw std::runtime_error("Column '" + name + "' is from DataFrame '" +
+                             df_name + "', but such a DataFrame is not known.");
+  }
+
+  const auto role = it->second.role(name);
+
+  if (role == containers::DataFrame::ROLE_CATEGORICAL) {
+    return to_view(it->second.int_column(name, role), categories_);
+  }
+
+  if (role == containers::DataFrame::ROLE_JOIN_KEY) {
+    return to_view(it->second.int_column(name, role), join_keys_encoding_);
+  }
+
+  if (role == containers::DataFrame::ROLE_TEXT) {
+    return to_view(it->second.text(name));
+  }
+
+  if (role == containers::DataFrame::ROLE_UNUSED ||
+      role == containers::DataFrame::ROLE_UNUSED_STRING) {
+    return to_view(it->second.unused_string(name));
+  }
+
+  throw std::runtime_error(
+      "Column '" + name + "' from DataFrame '" + df_name +
+      "' is expected to be a StringColumn, but it appears to be a "
+      "FloatColumn. You have most likely changed the type when "
+      "assigning a new role.");
+}
+
+// ----------------------------------------------------------------------------
+
 containers::ColumnView<strings::String> CatOpParser::numerical_as_string(
     const Poco::JSON::Object& _col) const {
   const auto obj = *JSON::get_object(_col, "operand1_");
@@ -132,55 +161,40 @@ containers::ColumnView<strings::String> CatOpParser::numerical_as_string(
 // ----------------------------------------------------------------------------
 
 containers::ColumnView<strings::String> CatOpParser::parse(
-    const Poco::JSON::Object& _col) const {
-  const auto type = JSON::get_value<std::string>(_col, "type_");
+    const commands::StringColumnOrStringColumnView& _cmd) const {
+  const auto handle =
+      [this](const auto& _cmd) -> containers::ColumnView<strings::String> {
+    using Type = std::decay_t<decltype(_cmd)>;
 
-  if (type == STRING_COLUMN) {
-    const auto name = JSON::get_value<std::string>(_col, "name_");
-
-    const auto df_name = JSON::get_value<std::string>(_col, "df_name_");
-
-    const auto it = data_frames_->find(df_name);
-
-    if (it == data_frames_->end()) {
-      throw std::runtime_error("Column '" + name + "' is from DataFrame '" +
-                               df_name +
-                               "', but such a DataFrame is not known.");
+    if constexpr (std::is_same<Type, StringBinaryOp>()) {
+      return binary_operation(_cmd);
     }
 
-    const auto role = it->second.role(name);
-
-    if (role == containers::DataFrame::ROLE_CATEGORICAL) {
-      return to_view(it->second.int_column(name, role), categories_);
+    if constexpr (std::is_same<Type, StringColumnOp>()) {
+      return get_column(_cmd);
     }
 
-    if (role == containers::DataFrame::ROLE_JOIN_KEY) {
-      return to_view(it->second.int_column(name, role), join_keys_encoding_);
+    if constexpr (std::is_same<Type, StringConstOp>()) {
+      const auto val = fct::get<"value_">(_cmd);
+      return containers::ColumnView<strings::String>::from_value(val);
     }
 
-    if (role == containers::DataFrame::ROLE_TEXT) {
-      return to_view(it->second.text(name));
+    if constexpr (std::is_same<Type, StringSubstringOp>()) {
+      return substring(_cmd);
     }
 
-    if (role == containers::DataFrame::ROLE_UNUSED ||
-        role == containers::DataFrame::ROLE_UNUSED_STRING) {
-      return to_view(it->second.unused_string(name));
+    if constexpr (std::is_same<Type, StringWithSubrolesOp>()) {
+      return with_subroles(_cmd);
     }
 
-    throw std::runtime_error(
-        "Column '" + name + "' from DataFrame '" + df_name +
-        "' is expected to be a StringColumn, but it appears to be a "
-        "FloatColumn. You have most likely changed the type when "
-        "assigning a new role.");
-  }
+    if constexpr (std::is_same<Type, StringWithUnitOp>()) {
+      return with_unit(_cmd);
+    }
+  };
 
-  const auto op = JSON::get_value<std::string>(_col, "operator_");
+  return std::visit(handle, _cmd.val_);
 
-  if (op == "const") {
-    const auto val =
-        strings::String(JSON::get_value<std::string>(_col, "value_"));
-    return containers::ColumnView<strings::String>::from_value(val);
-  }
+  /*
 
   if (type == STRING_COLUMN_VIEW && op == "with_subroles") {
     return with_subroles(_col);
@@ -205,7 +219,7 @@ containers::ColumnView<strings::String> CatOpParser::parse(
   throw std::runtime_error("Column of type '" + type +
                            "' not recognized for categorical columns.");
 
-  return unary_operation(_col);
+  return unary_operation(_col);*/
 }
 
 // ----------------------------------------------------------------------------
@@ -237,44 +251,38 @@ containers::ColumnView<strings::String> CatOpParser::subselection(
 
 // ----------------------------------------------------------------------------
 
+containers::ColumnView<strings::String> CatOpParser::substring(
+    const StringSubstringOp& _cmd) const {
+  const auto begin = _cmd.get<"begin_">();
+  const auto len = _cmd.get<"len_">();
+  const auto operand1 = parse(*_cmd.get<"operand1_">());
+  const auto substr = [begin,
+                       len](const strings::String& _val) -> strings::String {
+    return _val ? strings::String(_val.str().substr(begin, len)) : _val;
+  };
+  return containers::ColumnView<strings::String>::from_un_op(operand1, substr);
+}
+
+// ----------------------------------------------------------------------------
+
 containers::ColumnView<strings::String> CatOpParser::unary_operation(
-    const Poco::JSON::Object& _col) const {
-  const auto op = JSON::get_value<std::string>(_col, "operator_");
+    const StringUnaryOp& _cmd) const {
+  // TODO
 
-  const auto operand_type = JSON::get_value<std::string>(
-      *JSON::get_object(_col, "operand1_"), "type_");
+  /*  const auto op = JSON::get_value<std::string>(_col, "operator_");
 
-  const auto is_boolean = (operand_type == BOOLEAN_COLUMN_VIEW);
+const auto operand_type = JSON::get_value<std::string>(
+    *JSON::get_object(_col, "operand1_"), "type_");
 
-  if (is_boolean && op == "as_str") {
-    return boolean_as_string(_col);
-  }
+const auto is_boolean = (operand_type == BOOLEAN_COLUMN_VIEW);
 
-  if (!is_boolean && op == "as_str") {
-    return numerical_as_string(_col);
-  }
-
-  if (op == "categorical_value") {
-    return parse(*JSON::get_object(_col, "operand1_"));
-  }
-
-  if (op == "substr") {
-    const auto begin = JSON::get_value<size_t>(_col, "begin_");
-
-    const auto len = JSON::get_value<size_t>(_col, "len_");
-
-    const auto substr = [begin,
-                         len](const strings::String& _val) -> strings::String {
-      return _val ? strings::String(_val.str().substr(begin, len)) : _val;
-    };
-
-    return un_op(_col, substr);
-  }
-
-  throw std::runtime_error("Operator '" + op +
-                           "' not recognized for categorical columns.");
-
+if (is_boolean && op == "as_str") {
   return boolean_as_string(_col);
+}
+
+if (!is_boolean && op == "as_str") {
+  return numerical_as_string(_col);
+}*/
 }
 
 // ----------------------------------------------------------------------------
@@ -335,23 +343,18 @@ containers::ColumnView<strings::String> CatOpParser::update(
 // ----------------------------------------------------------------------------
 
 containers::ColumnView<strings::String> CatOpParser::with_subroles(
-    const Poco::JSON::Object& _col) const {
-  const auto col = parse(*JSON::get_object(_col, "operand1_"));
-
-  const auto subroles =
-      JSON::array_to_vector<std::string>(JSON::get_array(_col, "subroles_"));
-
+    const StringWithSubrolesOp& _cmd) const {
+  const auto col = parse(*_cmd.get<"operand1_">());
+  const auto subroles = _cmd.get<"subroles_">();
   return col.with_subroles(subroles);
 }
 
 // ----------------------------------------------------------------------------
 
 containers::ColumnView<strings::String> CatOpParser::with_unit(
-    const Poco::JSON::Object& _col) const {
-  const auto col = parse(*JSON::get_object(_col, "operand1_"));
-
-  const auto unit = JSON::get_value<std::string>(_col, "unit_");
-
+    const StringWithUnitOp& _cmd) const {
+  const auto col = parse(*_cmd.get<"operand1_">());
+  const auto unit = _cmd.get<"unit_">();
   return col.with_unit(unit);
 }
 
