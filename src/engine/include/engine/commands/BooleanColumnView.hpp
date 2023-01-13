7398251d9e07d7e5f5680c7bb6f42dcb228b5d16
Patrick Urbanke <patrick@getml.com>
2023-01-13 21:22:35 +0100

    Began transforming the BooleanOpParser

--- src/engine/include/engine/commands/BooleanColumnView.hpp
+++ src/engine/include/engine/commands/BooleanColumnView.hpp
@@ -10,17 +10,79 @@
 
 #include <variant>
 
-#include "engine/commands/BooleanBinaryOp.hpp"
-#include "engine/commands/BooleanConstOp.hpp"
-#include "engine/commands/BooleanSubselectionOp.hpp"
-#include "engine/commands/BooleanUnaryOp.hpp"
+#include "engine/Float.hpp"
+#include "engine/commands/FloatColumnOrFloatColumnView.hpp"
+#include "engine/commands/StringColumnOrStringColumnView.hpp"
+#include "fct/Field.hpp"
+#include "fct/Literal.hpp"
+#include "fct/NamedTuple.hpp"
+#include "fct/Ref.hpp"
 
 namespace engine {
 namespace commands {
 
-/// Defines a boolean column view.
-using BooleanColumnView = std::variant</*BooleanBinaryOp,*/ BooleanConstOp/*,
+class FloatColumnOrFloatColumnView;
+class StringColumnOrStringColumnView;
+
+class BooleanColumnView {
+ public:
+  /// The possible operators.
+  using BooleanBinaryOpLiteral =
+      fct::Literal<"and", /*"contains", "equal_to", "greater", "greater_equal",
+                   "less", "less_equal", "not_equal_to",*/
+                   "or", "xor">;
+
+  /// The command used for boolean binary operations.
+  using BooleanBinaryOp =
+      fct::NamedTuple<fct::Field<"operand1_", fct::Ref<BooleanColumnView>>,
+                      fct::Field<"operand2_", fct::Ref<BooleanColumnView>>,
+                      fct::Field<"operator_", BooleanBinaryOpLiteral>,
+                      fct::Field<"type_", fct::Literal<"BooleanColumnView">>>;
+
+  /// The possible operators.
+  using BooleanConstLiteral = fct::Literal<"const">;
+
+  /// The command used for boolean binary operations.
+  using BooleanConstOp =
+      fct::NamedTuple<fct::Field<"value_", bool>,
+                      fct::Field<"operator_", BooleanConstLiteral>,
+                      fct::Field<"type_", fct::Literal<"BooleanColumnView">>>;
+
+  /// The command used for boolean binary operations.
+  using BooleanNotOp =
+      fct::NamedTuple<fct::Field<"operand1_", fct::Ref<BooleanColumnView>>,
+                      fct::Field<"operator_", fct::Literal<"not">>,
+                      fct::Field<"type_", fct::Literal<"BooleanColumnView">>>;
+
+  /// The command used for boolean subselection operations.
+  using BooleanSubselectionOp = fct::NamedTuple<
+      fct::Field<"operand1_", fct::Ref<BooleanColumnView>>,
+      fct::Field<"operand2_",
+                 std::variant<fct::Ref<BooleanColumnView>,
+                              fct::Ref<FloatColumnOrFloatColumnView>>>,
+      fct::Field<"operator_", fct::Literal<"subselection">>,
+      fct::Field<"type_", fct::Literal<"BooleanColumnView">>>;
+  /// The command used to check whether a column is infinite.
+  using BooleanIsInfOp = fct::NamedTuple<
+      fct::Field<"operand1_", fct::Ref<FloatColumnOrFloatColumnView>>,
+      fct::Field<"operator_", fct::Literal<"is_inf">>,
+      fct::Field<"type_", fct::Literal<"BooleanColumnView">>>;
+
+  /// The command used to check whether a column is NaN or NULL.
+  using BooleanIsNullOp = fct::NamedTuple<
+      fct::Field<"operand1_",
+                 std::variant<fct::Ref<FloatColumnOrFloatColumnView>,
+                              fct::Ref<StringColumnOrStringColumnView>>>,
+      fct::Field<"operator_", fct::Literal<"is_nan", "is_null">>,
+      fct::Field<"type_", fct::Literal<"BooleanColumnView">>>;
+
+  /// Defines a boolean column view.
+  using RecursiveType = std::variant<BooleanBinaryOp, BooleanConstOp, BooleanIsInfOp,BooleanIsNullOp, BooleanNotOp/*,
                                        BooleanSubselectionOp, BooleanUnaryOp*/>;
+
+  /// Used to break the recursive definition.
+  RecursiveType val_;
+};
 
 }  // namespace commands
 }  // namespace engine
--- src/engine/include/engine/commands/FloatColumnOrFloatColumnView.hpp
+++ src/engine/include/engine/commands/FloatColumnOrFloatColumnView.hpp
@@ -8,13 +8,125 @@
 #ifndef ENGINE_COMMANDS_FLOATCOLUMNORFLOATCOLUMNVIEW_HPP_
 #define ENGINE_COMMANDS_FLOATCOLUMNORFLOATCOLUMNVIEW_HPP_
 
-#include "engine/commands/Column.hpp"
+#include <variant>
+
+#include "StringColumnOrStringColumnView.hpp"
+#include "engine/Float.hpp"
+#include "fct/Field.hpp"
+#include "fct/Literal.hpp"
+#include "fct/NamedTuple.hpp"
+#include "fct/Ref.hpp"
 
 namespace engine {
 namespace commands {
 
-using FloatColumnOrFloatColumnView =
-    typename Column::FloatColumnOrFloatColumnView;
+class StringColumnOrStringColumnView;
+
+class FloatColumnOrFloatColumnView {
+ public:
+  /// The command used for arange operations.
+  using FloatArangeOp =
+      fct::NamedTuple<fct::Field<"start_", Float>, fct::Field<"stop_", Float>,
+                      fct::Field<"step_", Float>,
+                      fct::Field<"operator_", fct::Literal<"arange">>,
+                      fct::Field<"type_", fct::Literal<"FloatColumnView">>>;
+
+  /// The command used for transforming string columns to float columns.
+  using FloatAsTSOp = fct::NamedTuple<
+      fct::Field<"operand1_", fct::Ref<StringColumnOrStringColumnView>>,
+      fct::Field<"operator_", fct::Literal<"as_ts">>,
+      fct::Field<"time_formats_", std::vector<std::string>>,
+      fct::Field<"type_", fct::Literal<"FloatColumnView">>>;
+
+  /// The possible operators.
+  using FloatBinaryOpLiteral =
+      fct::Literal<"divides", "fmod", "minus", "multiplies", "plus", "pow">;
+
+  /// The command used for float binary operations.
+  using FloatBinaryOp = fct::NamedTuple<
+      fct::Field<"operand1_", fct::Ref<FloatColumnOrFloatColumnView>>,
+      fct::Field<"operand2_", fct::Ref<FloatColumnOrFloatColumnView>>,
+      fct::Field<"operator_", FloatBinaryOpLiteral>,
+      fct::Field<"type_", fct::Literal<"FloatColumnView">>>;
+
+  /// The command used for retrieving float columns from a data frame.
+  using FloatColumnOp =
+      fct::NamedTuple<fct::Field<"df_name_", std::string>,
+                      fct::Field<"name_", std::string>,
+                      fct::Field<"type_", fct::Literal<"FloatColumn">>>;
+
+  /// The command used for float const operations.
+  using FloatConstOp =
+      fct::NamedTuple<fct::Field<"value_", Float>,
+                      fct::Field<"operator_", fct::Literal<"const">>,
+                      fct::Field<"type_", fct::Literal<"FloatColumnView">>>;
+
+  /// The command used for transforming string columns to float columns.
+  using FloatFromStringOp = fct::NamedTuple<
+      fct::Field<"operand1_", fct::Ref<StringColumnOrStringColumnView>>,
+      fct::Field<"operator_", fct::Literal<"as_num">>,
+      fct::Field<"type_", fct::Literal<"FloatColumnView">>>;
+
+  /// The command used for random operations.
+  using FloatRandomOp =
+      fct::NamedTuple<fct::Field<"seed_", unsigned int>,
+                      fct::Field<"operator_", fct::Literal<"random">>,
+                      fct::Field<"type_", fct::Literal<"FloatColumnView">>>;
+
+  /// The command used for float subselection operations.
+  using FloatSubselectionOp = fct::NamedTuple<
+      fct::Field<"operand1_", fct::Ref<FloatColumnOrFloatColumnView>>,
+      fct::Field<"operand2_", fct::Ref<FloatColumnOrFloatColumnView>>,
+      fct::Field<"operator_", fct::Literal<"subselection">>,
+      fct::Field<"type_", fct::Literal<"FloatColumnView">>>;
+
+  /// The possible operators.
+  using FloatUnaryOpLiteral =
+      fct::Literal<"abs", "acos", "asin", "atan",
+                   /*"boolean_as_num",*/ "cbrt", "ceil", "cos", "day", "erf",
+                   "exp", "floor", "hour", "lgamma", "log", "minute", "month",
+                   "round", "rowid", "second", "sin", "sqrt", "tan", "tgamma",
+                   "weekday", "year", "yearday">;
+
+  /// The command used for float unary operations.
+  using FloatUnaryOp = fct::NamedTuple<
+      fct::Field<"operand1_", fct::Ref<FloatColumnOrFloatColumnView>>,
+      fct::Field<"operator_", FloatUnaryOpLiteral>,
+      fct::Field<"type_", fct::Literal<"FloatColumnView">>>;
+
+  /// The command used for float binary operations.
+  using FloatUpdateOp = fct::NamedTuple<
+      fct::Field<"operand1_", fct::Ref<FloatColumnOrFloatColumnView>>,
+      fct::Field<"operand2_", fct::Ref<FloatColumnOrFloatColumnView>>,
+      fct::Field<"condition_",
+                 fct::Ref<FloatColumnOrFloatColumnView>>,  // TODO: This needs
+                                                           // to be a boolean
+                                                           // column view
+      fct::Field<"operator_", fct::Literal<"update">>,
+      fct::Field<"type_", fct::Literal<"FloatColumnView">>>;
+
+  /// The command used for string with subtoles operations.
+  using FloatWithSubrolesOp = fct::NamedTuple<
+      fct::Field<"operand1_", fct::Ref<FloatColumnOrFloatColumnView>>,
+      fct::Field<"subroles_", std::vector<std::string>>,
+      fct::Field<"type_", fct::Literal<"FloatColumnView">>>;
+
+  /// The command used for float with unit operations.
+  using FloatWithUnitOp = fct::NamedTuple<
+      fct::Field<"operand1_", fct::Ref<FloatColumnOrFloatColumnView>>,
+      fct::Field<"unit_", std::string>,
+      fct::Field<"type_", fct::Literal<"FloatColumnView">>>;
+
+  // TODO: Include FloatUpdateOp
+  using RecursiveType =
+      std::variant<FloatColumnOp, FloatArangeOp, FloatAsTSOp, FloatBinaryOp,
+                   FloatConstOp, FloatFromStringOp, FloatRandomOp,
+                   FloatSubselectionOp, FloatUnaryOp, FloatWithSubrolesOp,
+                   FloatWithUnitOp>;
+
+  /// Used to break the recursive definition.
+  RecursiveType val_;
+};
 
 }  // namespace commands
 }  // namespace engine
