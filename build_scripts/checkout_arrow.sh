b4129f2f1e752bfabcb93aa397659edb5e4c5fdc
Patrick Urbanke <patrick@getml.com>
2023-02-07 10:38:31 +0100

    Updated to Arrow 11.0; #349

--- build_scripts/checkout_arrow.sh
+++ build_scripts/checkout_arrow.sh
@@ -2,5 +2,5 @@ function checkout_arrow() {
     cd $DEPENDENCY_DIR || exit 1
     git clone https://github.com/apache/arrow.git 
     cd arrow || exit 1
-    git checkout apache-arrow-7.0.0 || exit 1
+    git checkout apache-arrow-11.0.0 || exit 1
 }
