5771b6bfeef8530c2d8dee5c3c1b152c672004bb
Patrick Urbanke <patrick@getml.com>
2023-02-19 20:08:39 +0100

    Made sure that empty Named Typle is possible

--- src/engine/include/fct/NamedTuple.hpp
+++ src/engine/include/fct/NamedTuple.hpp
@@ -312,24 +312,28 @@ class NamedTuple {
   constexpr static bool no_duplicate_field_names() {
     constexpr auto num_fields = std::tuple_size<Fields>{};
 
-    if constexpr (_i == num_fields) {
+    if constexpr (num_fields <= 1) {
       return true;
-    } else if constexpr (_j == -1) {
-      return no_duplicate_field_names<_i + 1, _i>();
     } else {
-      using FieldType1 =
-          std::decay_t<typename std::tuple_element<_i, Fields>::type>;
-      using FieldType2 =
-          std::decay_t<typename std::tuple_element<_j, Fields>::type>;
+      if constexpr (_i == num_fields) {
+        return true;
+      } else if constexpr (_j == -1) {
+        return no_duplicate_field_names<_i + 1, _i>();
+      } else {
+        using FieldType1 =
+            std::decay_t<typename std::tuple_element<_i, Fields>::type>;
+        using FieldType2 =
+            std::decay_t<typename std::tuple_element<_j, Fields>::type>;
 
-      constexpr auto field_name_i = FieldType1::name_;
-      constexpr auto field_name_j = FieldType2::name_;
+        constexpr auto field_name_i = FieldType1::name_;
+        constexpr auto field_name_j = FieldType2::name_;
 
-      constexpr bool no_duplicate = (field_name_i != field_name_j);
+        constexpr bool no_duplicate = (field_name_i != field_name_j);
 
-      static_assert(no_duplicate, "Duplicate field names are not allowed");
+        static_assert(no_duplicate, "Duplicate field names are not allowed");
 
-      return no_duplicate && no_duplicate_field_names<_i, _j - 1>();
+        return no_duplicate && no_duplicate_field_names<_i, _j - 1>();
+      }
     }
   }
 
