fc67f2184e9664a45d331a71cde68bc41adb04cc
Patrick Urbanke <patrick@getml.com>
2023-03-11 16:14:43 +0100

    Fixed two TODOs in the database command

--- src/engine/include/database/Command.hpp
+++ src/engine/include/database/Command.hpp
@@ -14,38 +14,41 @@
 
 #include "fct/Field.hpp"
 #include "fct/Literal.hpp"
-#include "fct/NamedTuple.hpp"
 #include "fct/TaggedUnion.hpp"
+#include "fct/define_named_tuple.hpp"
 
 namespace database {
 
 struct Command {
+  /// Needed by every command to create a new database.
+  using NewDBBasicOp =
+      fct::NamedTuple<fct::Field<"type_", fct::Literal<"Database.new">>,
+                      fct::Field<"conn_id_", std::string>>;
+
   /// The operation needed to create a MySQL connection.
-  using MySQLOp = fct::NamedTuple<
-      fct::Field<"db_", fct::Literal<"mysql", "mariadb">>,
-      fct::Field<"conn_id_", std::string>, fct::Field<"dbname_", std::string>,
-      fct::Field<"host_", std::string>, fct::Field<"port_", unsigned int>,
+  using MySQLOp = fct::define_named_tuple_t<
+      NewDBBasicOp, fct::Field<"db_", fct::Literal<"mysql", "mariadb">>,
+      fct::Field<"dbname_", std::string>, fct::Field<"host_", std::string>,
+      fct::Field<"port_", unsigned int>,
       fct::Field<"time_formats_", std::vector<std::string>>,
       fct::Field<"unix_socket_", std::string>,
       fct::Field<"user_", std::string>>;
 
   /// The operation needed to create a Postgres connection.
-  using PostgresOp =
-      fct::NamedTuple<fct::Field<"db_", fct::Literal<"postgres">>,
-                      fct::Field<"conn_id_", std::string>,
-                      fct::Field<"dbname_", std::string>,
-                      fct::Field<"host_", std::optional<std::string>>,
-                      fct::Field<"hostaddr_", std::optional<std::string>>,
-                      fct::Field<"port_", size_t>,
-                      fct::Field<"time_formats_", std::vector<std::string>>,
-                      fct::Field<"user_", std::string>>;
+  using PostgresOp = fct::define_named_tuple_t<
+      NewDBBasicOp, fct::Field<"db_", fct::Literal<"postgres">>,
+      fct::Field<"dbname_", std::string>,
+      fct::Field<"host_", std::optional<std::string>>,
+      fct::Field<"hostaddr_", std::optional<std::string>>,
+      fct::Field<"port_", size_t>,
+      fct::Field<"time_formats_", std::vector<std::string>>,
+      fct::Field<"user_", std::string>>;
 
   /// The operation needed to create an SQLITE3 connection.
-  using SQLite3Op =
-      fct::NamedTuple<fct::Field<"db_", fct::Literal<"sqlite3">>,
-                      fct::Field<"conn_id_", std::string>,
-                      fct::Field<"name_", std::string>,
-                      fct::Field<"time_formats_", std::vector<std::string>>>;
+  using SQLite3Op = fct::define_named_tuple_t<
+      NewDBBasicOp, fct::Field<"db_", fct::Literal<"sqlite3">>,
+      fct::Field<"name_", std::string>,
+      fct::Field<"time_formats_", std::vector<std::string>>>;
 
   using NamedTupleType =
       fct::TaggedUnion<"db_", MySQLOp, PostgresOp, SQLite3Op>;
