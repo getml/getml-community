215fc0ac9cfb240a96aa0dd8d972bab8d98f7711
Patrick Urbanke <patrick@getml.com>
2023-04-22 08:52:06 +0200

    Separate source files for the warnings

---
+++ src/engine/src/communication/Warnings.cpp
@@ -0,0 +1,23 @@
+// Copyright 2022 The SQLNet Company GmbH
+//
+// This file is licensed under the Elastic License 2.0 (ELv2).
+// Refer to the LICENSE.txt file in the root of the repository
+// for details.
+//
+
+#include "communication/Warnings.hpp"
+
+#include "communication/Sender.hpp"
+#include "json/json.hpp"
+
+namespace communication {
+
+void Warnings::send(Poco::Net::StreamSocket* _socket) const {
+  using NamedTupleType = fct::NamedTuple<
+      fct::Field<"warnings_", fct::Ref<const std::vector<std::string>>>>;
+  const auto named_tuple =
+      NamedTupleType(fct::make_field<"warnings_">(warnings_));
+  Sender::send_string(json::to_json(named_tuple), _socket);
+}
+
+}  // namespace communication
