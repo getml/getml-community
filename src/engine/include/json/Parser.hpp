0a1b023f2b36cef7d2ac1ac781e1764f4455166e
Patrick Urbanke <patrick@getml.com>
2023-05-24 20:34:17 +0200

    Removed SimdJSONParser

--- src/engine/include/json/Parser.hpp
+++ src/engine/include/json/Parser.hpp
@@ -13,7 +13,6 @@
 #include <Poco/JSON/Array.h>
 #include <Poco/JSON/Object.h>
 #include <Poco/JSON/Parser.h>
-#include <simdjson.h>
 #include <yyjson.h>
 
 #include <exception>
@@ -146,121 +145,6 @@ struct PocoJSONParser {
     const auto iota = fct::iota<size_t>(0, (*_arr)->size());
     const auto get = [_arr](const size_t _i) { return (*_arr)->get(_i); };
     return fct::collect::vector(iota | VIEWS::transform(get));
-  }
-};
-
-struct SimdJSONParser {
-  using InputArrayType = simdjson::ondemand::array;
-  using InputObjectType = simdjson::ondemand::object;
-  using InputVarType = simdjson::ondemand::value;
-
-  using OutputArrayType = Poco::JSON::Array::Ptr;
-  using OutputObjectType = Poco::JSON::Object::Ptr;
-  using OutputVarType = Poco::Dynamic::Var;
-
-  static void add(const OutputVarType _var, OutputArrayType* _arr) noexcept {
-    (*_arr)->add(_var);
-  }
-
-  static OutputVarType empty_var() noexcept { return Poco::Dynamic::Var(); }
-
-  static InputVarType from_string(const std::string& _str) {
-    simdjson::padded_string padded_str(_str);
-    simdjson::ondemand::parser parser;
-    return parser.iterate(padded_str);
-  }
-
-  static fct::Result<InputVarType> get_field(const std::string& _name,
-                                             InputObjectType* _obj) noexcept {
-    auto res = (*_obj)[_name];
-    InputVarType v;
-    const auto error = res.get(v);
-    if (error) {
-      return fct::Error("Object contains no field named '" + _name + "'.");
-    }
-    return v;
-  }
-
-  static OutputArrayType new_array() noexcept {
-    return Poco::JSON::Array::Ptr(new Poco::JSON::Array());
-  }
-
-  static OutputObjectType new_object() noexcept {
-    return Poco::JSON::Object::Ptr(new Poco::JSON::Object());
-  }
-
-  static bool is_empty(InputVarType* _var) noexcept { return _var->is_null(); }
-
-  static bool is_empty(OutputVarType* _var) noexcept { return _var->isEmpty(); }
-
-  static void set_field(const std::string& _name, const OutputVarType& _var,
-                        OutputObjectType* _obj) noexcept {
-    (*_obj)->set(_name, _var);
-  }
-
-  template <class T>
-  static fct::Result<T> to_basic_type(InputVarType* _var) noexcept {
-    if constexpr (std::is_same<std::decay_t<T>, std::string>()) {
-      auto r = _var->get_string();
-      if (r.error()) {
-        return fct::Error("Could not cast to string.");
-      }
-      return std::string(r.value());
-    } else if constexpr (std::is_same<std::decay_t<T>, bool>()) {
-      auto r = _var->get_bool();
-      if (r.error()) {
-        return fct::Error("Could not cast to boolean.");
-      }
-      return r.value();
-    } else if constexpr (std::is_floating_point<std::decay_t<T>>()) {
-      auto r = _var->get_double();
-      if (r.error()) {
-        return fct::Error("Could not cast to double.");
-      }
-      return static_cast<T>(r.value());
-    } else {
-      auto r = _var->get_int64();
-      if (r.error()) {
-        return fct::Error("Could not cast to integer.");
-      }
-      return static_cast<T>(r.value());
-    }
-  }
-
-  static fct::Result<InputArrayType> to_array(InputVarType* _var) noexcept {
-    auto r = _var->get_array();
-    if (r.error()) {
-      return fct::Error("Could not cast to array!");
-    }
-    return r.value();
-  }
-
-  static std::map<std::string, InputVarType> to_map(
-      InputObjectType* _obj) noexcept {
-    std::map<std::string, InputVarType> m;
-    for (auto field : *_obj) {
-      std::string_view name = field.unescaped_key();
-      m[std::string(name)] = field.value();
-    }
-    return m;
-  }
-
-  static fct::Result<InputObjectType> to_object(InputVarType* _var) noexcept {
-    auto r = _var->get_object();
-    if (r.error()) {
-      return fct::Error("Could not cast to object!");
-    }
-    return r.value();
-  }
-
-  static std::vector<InputVarType> to_vec(InputArrayType* _arr) noexcept {
-    std::vector<InputVarType> vec;
-    for (auto elem : *_arr) {
-      if (!elem.error()) {
-        vec.push_back(elem.value());
-      }
-    }
-    return vec;
   }
 };
 
