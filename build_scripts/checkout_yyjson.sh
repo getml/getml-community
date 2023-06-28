55152a88ba17edd407e15c0b38dc80b2dc253a78
Patrick Urbanke <patrick@getml.com>
2023-06-28 11:15:20 +0200

    Use yyjson 0.7.0

--- build_scripts/checkout_yyjson.sh
+++ build_scripts/checkout_yyjson.sh
@@ -2,5 +2,5 @@ function checkout_yyjson() {
     cd $DEPENDENCY_DIR || exit 1
     git clone https://github.com/ibireme/yyjson.git
     cd yyjson || exit 1
-    git checkout 0.6.0 || exit 1
+    git checkout 0.7.0 || exit 1
 }
