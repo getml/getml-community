f11db277fec7157ade6b4082883fdd4268eff99a
Patrick Urbanke <patrick@getml.com>
2023-06-28 11:11:05 +0200

    Improved includes

--- src/engine/include/fastprop/algorithm/algorithm.hpp
+++ src/engine/include/fastprop/algorithm/algorithm.hpp
@@ -1,18 +1,15 @@
 // Copyright 2022 The SQLNet Company GmbH
-// 
-// This file is licensed under the Elastic License 2.0 (ELv2). 
-// Refer to the LICENSE.txt file in the root of the repository 
+//
+// This file is licensed under the Elastic License 2.0 (ELv2).
+// Refer to the LICENSE.txt file in the root of the repository
 // for details.
-// 
+//
 
 #ifndef FASTPROP_ALGORITHM_ALGORITHM_HPP_
 #define FASTPROP_ALGORITHM_ALGORITHM_HPP_
 
-#include "fastprop/algorithm/Aggregator.hpp"
 #include "fastprop/algorithm/FastProp.hpp"
 #include "fastprop/algorithm/FitParams.hpp"
-#include "fastprop/algorithm/TableHolder.hpp"
-#include "fastprop/algorithm/TableHolderParams.hpp"
 #include "fastprop/algorithm/TransformParams.hpp"
 
 #endif  // FASTPROP_ALGORITHM_ALGORITHM_HPP_
