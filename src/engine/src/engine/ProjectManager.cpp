6787360cf42b8ef6a17aa449bc0fa438b4ca4837
Patrick Urbanke <patrick@getml.com>
2023-09-03 08:37:51 +0200

    Made the format_ optional, defaulting to flexbuffers

--- src/engine/src/engine/ProjectManager.cpp
+++ src/engine/src/engine/ProjectManager.cpp
@@ -399,9 +399,14 @@ void ProjectManager::save_pipeline(const typename Command::SavePipelineOp& _cmd,
 
   const auto path = project_directory() + "pipelines/";
 
+  using Format = typename helpers::Saver::Format;
+
+  const auto format =
+      _cmd.get<"format_">().value_or(Format::make<"flexbuffers">());
+
   const auto params = pipelines::SaveParams(
       fct::make_field<"categories_">(categories().strings()) *
-      fct::make_field<"fitted_">(*fitted) * _cmd.get_field<"format_">() *
+      fct::make_field<"fitted_">(*fitted) * fct::make_field<"format_">(format) *
       fct::make_field<"name_">(name) * fct::make_field<"path_">(path) *
       fct::make_field<"pipeline_">(pipeline) *
       fct::make_field<"temp_dir_">(params_.options_.temp_dir()));
