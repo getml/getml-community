e578aab0a2905d6c1799a36f25c846fbdeeccc3a
Patrick Urbanke <patrick@getml.com>
2023-09-06 23:25:41 +0200

    Rewrote the DatabaseCommand

--- src/engine/include/commands/DatabaseCommand.hpp
+++ src/engine/include/commands/DatabaseCommand.hpp
@@ -24,97 +24,116 @@ namespace commands {
 /// Any command to be handled by the DatabaseHandler.
 struct DatabaseCommand {
   /// The operation needed to copy a table.
-  using CopyTableOp =
-      rfl::NamedTuple<rfl::Field<"type_", rfl::Literal<"Database.copy_table">>,
-                      rfl::Field<"source_conn_id_", std::string>,
-                      rfl::Field<"source_table_", std::string>,
-                      rfl::Field<"target_conn_id_", std::string>,
-                      rfl::Field<"target_table_", std::string>>;
+  struct CopyTableOp {
+    rfl::Field<"type_", rfl::Literal<"Database.copy_table">> type;
+    rfl::Field<"source_conn_id_", std::string> source_conn_id;
+    rfl::Field<"source_table_", std::string> source_table;
+    rfl::Field<"target_conn_id_", std::string> target_conn_id;
+    rfl::Field<"target_table_", std::string> target_table;
+  };
 
   /// The operation needed to describe a connection.
-  using DescribeConnectionOp = rfl::NamedTuple<
-      rfl::Field<"type_", rfl::Literal<"Database.describe_connection">>,
-      rfl::Field<"name_", std::string>>;
+  struct DescribeConnectionOp {
+    rfl::Field<"type_", rfl::Literal<"Database.describe_connection">> type;
+    rfl::Field<"name_", std::string> name;
+  };
 
   /// The operation needed to drop a table.
-  using DropTableOp =
-      rfl::NamedTuple<rfl::Field<"type_", rfl::Literal<"Database.drop_table">>,
-                      rfl::Field<"conn_id_", std::string>,
-                      rfl::Field<"name_", std::string>>;
+  struct DropTableOp {
+    rfl::Field<"type_", rfl::Literal<"Database.drop_table">> type;
+    rfl::Field<"conn_id_", std::string> conn_id;
+    rfl::Field<"name_", std::string> name;
+  };
 
   /// The operation needed to execute a query.
-  using ExecuteOp =
-      rfl::NamedTuple<rfl::Field<"type_", rfl::Literal<"Database.execute">>,
-                      rfl::Field<"name_", std::string>>;
+  struct ExecuteOp {
+    rfl::Field<"type_", rfl::Literal<"Database.execute">> type;
+    rfl::Field<"name_", std::string> name;
+  };
 
   /// The operation needed to get a table.
-  using GetOp =
-      rfl::NamedTuple<rfl::Field<"type_", rfl::Literal<"Database.get">>,
-                      rfl::Field<"name_", std::string>>;
+  struct GetOp {
+    rfl::Field<"type_", rfl::Literal<"Database.get">> type;
+    rfl::Field<"name_", std::string> name;
+  };
 
   /// The operation needed to get colnames.
-  using GetColnamesOp = rfl::NamedTuple<
-      rfl::Field<"type_", rfl::Literal<"Database.get_colnames">>,
-      rfl::Field<"conn_id_", std::string>, rfl::Field<"name_", std::string>,
-      rfl::Field<"query_", std::optional<std::string>>>;
+  struct GetColnamesOp {
+    rfl::Field<"type_", rfl::Literal<"Database.get_colnames">> type;
+    rfl::Field<"conn_id_", std::string> conn_id;
+    rfl::Field<"name_", std::string> name;
+    rfl::Field<"query_", std::optional<std::string>> query;
+  };
 
   /// The operation needed to get the content of a table.
-  using GetContentOp =
-      rfl::NamedTuple<rfl::Field<"type_", rfl::Literal<"Database.get_content">>,
-                      rfl::Field<"name_", std::string>,
-                      rfl::Field<"conn_id_", std::string>,
-                      rfl::Field<"draw_", Int>, rfl::Field<"length_", Int>,
-                      rfl::Field<"start_", Int>>;
+  struct GetContentOp {
+    rfl::Field<"type_", rfl::Literal<"Database.get_content">> type;
+    rfl::Field<"name_", std::string> name;
+    rfl::Field<"conn_id_", std::string> conn_id;
+    rfl::Field<"draw_", Int> draw;
+    rfl::Field<"length_", Int> length;
+    rfl::Field<"start_", Int> start;
+  };
 
   /// The operation needed to the number of rows.
-  using GetNRowsOp =
-      rfl::NamedTuple<rfl::Field<"type_", rfl::Literal<"Database.get_nrows">>,
-                      rfl::Field<"conn_id_", std::string>,
-                      rfl::Field<"name_", std::string>>;
+  struct GetNRowsOp {
+    rfl::Field<"type_", rfl::Literal<"Database.get_nrows">> type;
+    rfl::Field<"conn_id_", std::string> conn_id;
+    rfl::Field<"name_", std::string> name;
+  };
 
   /// The operation needed to list the connections.
-  using ListConnectionsOp = rfl::NamedTuple<
-      rfl::Field<"type_", rfl::Literal<"Database.list_connections">>>;
+  struct ListConnectionsOp {
+    rfl::Field<"type_", rfl::Literal<"Database.list_connections">> type;
+  };
 
   /// The operation needed to list the tables.
-  using ListTablesOp =
-      rfl::NamedTuple<rfl::Field<"type_", rfl::Literal<"Database.list_tables">>,
-                      rfl::Field<"name_", std::string>>;
+  struct ListTablesOp {
+    rfl::Field<"type_", rfl::Literal<"Database.list_tables">> type;
+    rfl::Field<"name_", std::string> name;
+  };
 
   /// The operation needed to create a new connection.
   using NewDBOp = typename database::Command::NamedTupleType;
 
   /// The operation needed to read data from a CSV file.
-  using ReadCSVOp = rfl::NamedTuple<
-      rfl::Field<"type_", rfl::Literal<"Database.read_csv">>,
-      rfl::Field<"colnames_", std::optional<std::vector<std::string>>>,
-      rfl::Field<"conn_id_", std::string>,
-      rfl::Field<"fnames_", std::vector<std::string>>,
-      rfl::Field<"name_", std::string>, rfl::Field<"num_lines_read_", size_t>,
-      rfl::Field<"quotechar_", std::string>, rfl::Field<"sep_", std::string>,
-      rfl::Field<"skip_", size_t>>;
+  struct ReadCSVOp {
+    rfl::Field<"type_", rfl::Literal<"Database.read_csv">> type;
+    rfl::Field<"colnames_", std::optional<std::vector<std::string>>> colnames;
+    rfl::Field<"conn_id_", std::string> conn_id;
+    rfl::Field<"fnames_", std::vector<std::string>> fnames;
+    rfl::Field<"name_", std::string> name;
+    rfl::Field<"num_lines_read_", size_t> num_lines_read;
+    rfl::Field<"quotechar_", std::string> quotechar;
+    rfl::Field<"sep_", std::string> sep;
+    rfl::Field<"skip_", size_t> skip;
+  };
 
   /// The operation needed to refresh the contents of the database.
-  using RefreshOp =
-      rfl::NamedTuple<rfl::Field<"type_", rfl::Literal<"Database.refresh">>>;
+  struct RefreshOp {
+    rfl::Field<"type_", rfl::Literal<"Database.refresh">> type;
+  };
 
   /// The operation needed to sniff the data in a CSV file.
-  using SniffCSVOp = rfl::NamedTuple<
-      rfl::Field<"type_", rfl::Literal<"Database.sniff_csv">>,
-      rfl::Field<"colnames_", std::optional<std::vector<std::string>>>,
-      rfl::Field<"conn_id_", std::string>,
-      rfl::Field<"dialect_", std::optional<rfl::Literal<"python">>>,
-      rfl::Field<"fnames_", std::vector<std::string>>,
-      rfl::Field<"name_", std::string>,
-      rfl::Field<"num_lines_sniffed_", size_t>,
-      rfl::Field<"quotechar_", std::string>, rfl::Field<"sep_", std::string>,
-      rfl::Field<"skip_", size_t>>;
+  struct SniffCSVOp {
+    rfl::Field<"type_", rfl::Literal<"Database.sniff_csv">> type;
+    rfl::Field<"colnames_", std::optional<std::vector<std::string>>> colnames;
+    rfl::Field<"conn_id_", std::string> conn_id;
+    rfl::Field<"dialect_", std::optional<rfl::Literal<"python">>> dialect;
+    rfl::Field<"fnames_", std::vector<std::string>> fnames;
+    rfl::Field<"name_", std::string> name;
+    rfl::Field<"num_lines_sniffed_", size_t> num_lines_sniffed;
+    rfl::Field<"quotechar_", std::string> quotechar;
+    rfl::Field<"sep_", std::string> sep;
+    rfl::Field<"skip_", size_t> skip;
+  };
 
   /// The operation needed to sniff a table in the database.
-  using SniffTableOp =
-      rfl::NamedTuple<rfl::Field<"type_", rfl::Literal<"Database.sniff_table">>,
-                      rfl::Field<"conn_id_", std::string>,
-                      rfl::Field<"name_", std::string>>;
+  struct SniffTableOp {
+    rfl::Field<"type_", rfl::Literal<"Database.sniff_table">> type;
+    rfl::Field<"conn_id_", std::string> conn_id;
+    rfl::Field<"name_", std::string> name;
+  };
 
   using NamedTupleType =
       rfl::TaggedUnion<"type_", CopyTableOp, DescribeConnectionOp, DropTableOp,
