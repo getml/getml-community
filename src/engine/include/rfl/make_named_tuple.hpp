9467a1d34bf16a4bd55fd2094d20a6e061705123
Patrick Urbanke <patrick@getml.com>
2023-09-10 23:31:17 +0200

    Added another implementation of make_named_tuple

--- src/engine/include/rfl/make_named_tuple.hpp
+++ src/engine/include/rfl/make_named_tuple.hpp
@@ -17,9 +17,16 @@ namespace rfl {
 /// Convenience constructor that doesn't require you
 /// to explitly defined the field types.
 template <class... FieldTypes>
-auto make_named_tuple(FieldTypes... _args) {
+auto make_named_tuple(FieldTypes&&... _args) {
   return NamedTuple<std::decay_t<FieldTypes>...>(
       std::forward<FieldTypes>(_args)...);
+}
+
+/// Convenience constructor that doesn't require you
+/// to explitly defined the field types.
+template <class Head, class... Tail>
+auto make_named_tuple(const Head& _h, const Tail&... _t) {
+  return NamedTuple<Head, Tail...>(_h, _t...);
 }
 
 }  // namespace rfl
