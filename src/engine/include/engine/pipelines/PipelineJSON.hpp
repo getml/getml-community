720db8c5f6f434ee4912cf5da2023e63b7fb0c01
Patrick Urbanke <patrick@getml.com>
2023-09-22 22:03:35 +0200

    rewrote PipelineJSON and Fingerprints

--- src/engine/include/engine/pipelines/PipelineJSON.hpp
+++ src/engine/include/engine/pipelines/PipelineJSON.hpp
@@ -14,41 +14,44 @@
 #include "engine/pipelines/Fingerprints.hpp"
 #include "helpers/Schema.hpp"
 #include "rfl/Field.hpp"
+#include "rfl/Flatten.hpp"
 #include "rfl/Ref.hpp"
-#include "rfl/define_named_tuple.hpp"
 
 namespace engine {
 namespace pipelines {
 
-using PipelineJSON = rfl::define_named_tuple_t<
+struct PipelineJSON {
+  /// The fingerprints required for the fitted pipeline.
+  rfl::Flatten<Fingerprints> fingerprints;
 
-    /// The fingerprints required for the fitted pipeline.
-    Fingerprints,
+  /// Whether the pipeline is allowed to handle HTTP requests.
+  rfl::Field<"allow_http_", bool> allow_http;
 
-    /// Whether the pipeline is allowed to handle HTTP requests.
-    rfl::Field<"allow_http_", bool>,
+  /// Date and time of creation, expressed as a string
+  rfl::Field<"creation_time_", std::string> creation_time;
 
-    /// Date and time of creation, expressed as a string
-    rfl::Field<"creation_time_", std::string>,
+  /// The schema of the peripheral tables as they are inserted into the
+  /// feature learners.
+  rfl::Field<"modified_peripheral_schema_",
+             rfl::Ref<const std::vector<helpers::Schema>>>
+      modified_peripheral_schema;
 
-    /// The schema of the peripheral tables as they are inserted into the
-    /// feature learners.
-    rfl::Field<"modified_peripheral_schema_",
-               rfl::Ref<const std::vector<helpers::Schema>>>,
+  /// The schema of the population as it is inserted into the feature
+  /// learners.
+  rfl::Field<"modified_population_schema_", rfl::Ref<const helpers::Schema>>
+      modified_population_schema;
 
-    /// The schema of the population as it is inserted into the feature
-    /// learners.
-    rfl::Field<"modified_population_schema_", rfl::Ref<const helpers::Schema>>,
+  /// The schema of the peripheral tables as they are originally passed.
+  rfl::Field<"peripheral_schema_", rfl::Ref<const std::vector<helpers::Schema>>>
+      peripheral_schema;
 
-    /// The schema of the peripheral tables as they are originally passed.
-    rfl::Field<"peripheral_schema_",
-               rfl::Ref<const std::vector<helpers::Schema>>>,
+  /// The schema of the population table as originally passed.
+  rfl::Field<"population_schema_", rfl::Ref<const helpers::Schema>>
+      population_schema;
 
-    /// The schema of the population table as originally passed.
-    rfl::Field<"population_schema_", rfl::Ref<const helpers::Schema>>,
-
-    /// The names of the targets.
-    rfl::Field<"targets_", std::vector<std::string>>>;
+  /// The names of the targets.
+  rfl::Field<"targets_", std::vector<std::string>> targets;
+};
 
 }  // namespace pipelines
 }  // namespace engine
--- src/engine/src/engine/load_fitted.cpp
+++ src/engine/src/engine/load_fitted.cpp
@@ -88,13 +88,13 @@ Pipeline load_fitted(const std::string& _path, const Pipeline& _pipeline,
       rfl::Ref<const pipelines::FittedPipeline>::make(pipelines::FittedPipeline{
           .feature_learners_ = feature_learners,
           .feature_selectors_ = feature_selectors,
-          .fingerprints_ = pipeline_json,
+          .fingerprints_ = pipeline_json.fingerprints(),
           .modified_peripheral_schema_ =
-              pipeline_json.get<"modified_peripheral_schema_">(),
+              pipeline_json.modified_peripheral_schema(),
           .modified_population_schema_ =
-              pipeline_json.get<"modified_population_schema_">(),
-          .peripheral_schema_ = pipeline_json.get<"peripheral_schema_">(),
-          .population_schema_ = pipeline_json.get<"population_schema_">(),
+              pipeline_json.modified_population_schema(),
+          .peripheral_schema_ = pipeline_json.peripheral_schema(),
+          .population_schema_ = pipeline_json.population_schema(),
           .predictors_ = predictors,
           .preprocessors_ = preprocessors});
 
@@ -112,19 +112,18 @@ load_feature_learners(const std::string& _path,
 
   const featurelearners::FeatureLearnerParams feature_learner_params =
       rfl::make_field<"dependencies_">(
-          _pipeline_json.get<"preprocessor_fingerprints_">()) *
+          _pipeline_json.fingerprints().preprocessor_fingerprints()) *
       rfl::make_field<"peripheral_">(peripheral) *
       rfl::make_field<"peripheral_schema_">(
-          _pipeline_json.get<"modified_peripheral_schema_">()) *
+          _pipeline_json.modified_peripheral_schema()) *
       rfl::make_field<"placeholder_">(placeholder) *
       rfl::make_field<"population_schema_">(
-          _pipeline_json.get<"modified_population_schema_">()) *
+          _pipeline_json.modified_population_schema()) *
       rfl::make_field<"target_num_">(
           featurelearners::AbstractFeatureLearner::USE_ALL_TARGETS);
 
-  const auto feature_learners =
-      fit::init_feature_learners(_pipeline, feature_learner_params,
-                                 _pipeline_json.get<"targets_">().size());
+  const auto feature_learners = fit::init_feature_learners(
+      _pipeline, feature_learner_params, _pipeline_json.targets().size());
 
   for (size_t i = 0; i < feature_learners.size(); ++i) {
     auto& fe = feature_learners.at(i);
@@ -144,8 +143,8 @@ Predictors load_feature_selectors(
     const PipelineJSON& _pipeline_json, const Pipeline& _pipeline) {
   const auto feature_selectors = fit::init_predictors(
       _pipeline, Purpose::make<"feature_selectors_">(), _feature_selector_impl,
-      *_pipeline_json.get<"fl_fingerprints_">(),
-      _pipeline_json.get<"targets_">().size());
+      *_pipeline_json.fingerprints().fl_fingerprints(),
+      _pipeline_json.targets().size());
 
   for (size_t i = 0; i < feature_selectors.size(); ++i) {
     for (size_t j = 0; j < feature_selectors.at(i).size(); ++j) {
@@ -185,8 +184,8 @@ Predictors load_predictors(
     const PipelineJSON& _pipeline_json, const Pipeline& _pipeline) {
   const auto predictors = fit::init_predictors(
       _pipeline, Purpose::make<"predictors_">(), _predictor_impl,
-      *_pipeline_json.get<"fs_fingerprints_">(),
-      _pipeline_json.get<"targets_">().size());
+      *_pipeline_json.fingerprints().fs_fingerprints(),
+      _pipeline_json.targets().size());
 
   for (size_t i = 0; i < predictors.size(); ++i) {
     for (size_t j = 0; j < predictors.at(i).size(); ++j) {
@@ -208,7 +207,7 @@ std::vector<rfl::Ref<const preprocessors::Preprocessor>> load_preprocessors(
     const rfl::Ref<dependency::PreprocessorTracker> _preprocessor_tracker,
     const PipelineJSON& _pipeline_json, const Pipeline& _pipeline) {
   auto preprocessors = fit::init_preprocessors(
-      _pipeline, _pipeline_json.get<"df_fingerprints_">());
+      _pipeline, _pipeline_json.fingerprints().df_fingerprints());
 
   for (size_t i = 0; i < preprocessors.size(); ++i) {
     const auto& p = preprocessors.at(i);
