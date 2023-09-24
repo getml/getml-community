86d911ad6d0af5daa6b89650d2c1d1304d968539
Patrick Urbanke <patrick@getml.com>
2023-09-24 16:26:10 +0200

    Rewrote the FitPredictorsParams

--- src/engine/include/engine/pipelines/FitPredictorsParams.hpp
+++ src/engine/include/engine/pipelines/FitPredictorsParams.hpp
@@ -26,47 +26,52 @@
 namespace engine {
 namespace pipelines {
 
-using FitPredictorsParams = rfl::NamedTuple<
+struct FitPredictorsParams {
+  /// A pointer to the autofeatures. This is modifiable on purpose, because we
+  /// want to be able to cache generated features.
+  rfl::Field<"autofeatures_", containers::NumericalFeatures*> autofeatures;
 
-    /// A pointer to the autofeatures. This is modifiable on purpose, because we
-    /// want to be able to cache generated features.
-    rfl::Field<"autofeatures_", containers::NumericalFeatures*>,
+  /// The dependencies for the predictors (either fl_fingerprints or
+  /// fs_fingerprints)
+  rfl::Field<"dependencies_",
+             rfl::Ref<const std::vector<commands::Fingerprint>>>
+      dependencies;
 
-    /// The dependencies for the predictors (either fl_fingerprints or
-    /// fs_fingerprints)
-    rfl::Field<"dependencies_",
-               rfl::Ref<const std::vector<commands::Fingerprint>>>,
+  /// The feature learners used in this pipeline.
+  rfl::Field<
+      "feature_learners_",
+      std::vector<rfl::Ref<const featurelearners::AbstractFeatureLearner>>>
+      feature_learners;
 
-    /// The feature learners used in this pipeline.
-    rfl::Field<
-        "feature_learners_",
-        std::vector<rfl::Ref<const featurelearners::AbstractFeatureLearner>>>,
+  /// The parameters passed to fit(...)
+  rfl::Field<"fit_params_", FitParams> fit_params;
 
-    /// The parameters passed to fit(...)
-    rfl::Field<"fit_params_", FitParams>,
+  /// The underlying impl_
+  rfl::Field<"impl_", rfl::Ref<const predictors::PredictorImpl>> impl;
 
-    /// The underlying impl_
-    rfl::Field<"impl_", rfl::Ref<const predictors::PredictorImpl>>,
+  /// The peripheral tables, after applying the staging and preprocessing.
+  rfl::Field<"peripheral_dfs_", std::vector<containers::DataFrame>>
+      peripheral_dfs;
 
-    /// The peripheral tables, after applying the staging and preprocessing.
-    rfl::Field<"peripheral_dfs_", std::vector<containers::DataFrame>>,
+  /// The underlying pipeline
+  rfl::Field<"pipeline_", Pipeline> pipeline;
 
-    /// The underlying pipeline
-    rfl::Field<"pipeline_", Pipeline>,
+  /// The population table, after applying the staging and preprocessing.
+  rfl::Field<"population_df_", containers::DataFrame> population_df;
 
-    /// The population table, after applying the staging and preprocessing.
-    rfl::Field<"population_df_", containers::DataFrame>,
+  /// The preprocessors used in this pipeline.
+  rfl::Field<"preprocessors_",
+             std::vector<rfl::Ref<const preprocessors::Preprocessor>>>
+      preprocessors;
 
-    /// The preprocessors used in this pipeline.
-    rfl::Field<"preprocessors_",
-               std::vector<rfl::Ref<const preprocessors::Preprocessor>>>,
+  /// The fingerprints of the preprocessors used for fitting.
+  rfl::Field<"preprocessor_fingerprints_",
+             rfl::Ref<const std::vector<commands::Fingerprint>>>
+      preprocessor_fingerprints;
 
-    /// The fingerprints of the preprocessors used for fitting.
-    rfl::Field<"preprocessor_fingerprints_",
-               rfl::Ref<const std::vector<commands::Fingerprint>>>,
-
-    /// The purpose (feature_selector_ or predictor_)
-    rfl::Field<"purpose_", Purpose>>;
+  /// The purpose (feature_selector_ or predictor_)
+  rfl::Field<"purpose_", Purpose> purpose;
+};
 
 }  // namespace pipelines
 }  // namespace engine
