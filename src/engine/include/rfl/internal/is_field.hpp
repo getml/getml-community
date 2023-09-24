ca7546173f76a9879b5acc1dcbee3e400790e5fc
Patrick Urbanke <patrick@getml.com>
2023-09-24 15:01:21 +0200

    Added supports for naked fields in as and to_named_tuple

--- src/engine/include/rfl/internal/is_field.hpp
+++ src/engine/include/rfl/internal/is_field.hpp
@@ -5,29 +5,30 @@
 // for details.
 //
 
-#ifndef RFL_INTERNAL_ISFLATTENFIELD_HPP_
-#define RFL_INTERNAL_ISFLATTENFIELD_HPP_
+#ifndef RFL_INTERNAL_ISFIELD_HPP_
+#define RFL_INTERNAL_ISFIELD_HPP_
 
 #include <tuple>
 #include <type_traits>
 #include <utility>
 
-#include "rfl/Flatten.hpp"
+#include "rfl/Field.hpp"
+#include "rfl/internal/StringLiteral.hpp"
 
 namespace rfl {
 namespace internal {
 
 template <class T>
-class is_flatten_field;
+class is_field;
 
 template <class T>
-class is_flatten_field : public std::false_type {};
+class is_field : public std::false_type {};
+
+template <StringLiteral _name, class Type>
+class is_field<Field<_name, Type>> : public std::true_type {};
 
 template <class T>
-class is_flatten_field<Flatten<T>> : public std::true_type {};
-
-template <class T>
-constexpr bool is_flatten_field_v = is_flatten_field<T>::value;
+constexpr bool is_field_v = is_field<T>::value;
 
 }  // namespace internal
 }  // namespace rfl
