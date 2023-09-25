bac3162e0211473ffc7aa9ead52a7f21c3aa4a2f
Patrick Urbanke <patrick@getml.com>
2023-09-25 20:41:59 +0200

    Added rfl::Enum

--- src/engine/include/rfl/NamedTuple.hpp
+++ src/engine/include/rfl/NamedTuple.hpp
@@ -20,6 +20,7 @@
 #include "rfl/internal/find_index.hpp"
 #include "rfl/internal/has_named_tuple_method_v.hpp"
 #include "rfl/internal/has_named_tuple_type_v.hpp"
+#include "rfl/internal/no_duplicate_field_names.hpp"
 
 namespace rfl {
 
@@ -312,33 +313,8 @@ class NamedTuple {
   }
 
   /// We cannot allow duplicate field names.
-  template <int _i = 1, int _j = 0>
   constexpr static bool no_duplicate_field_names() {
-    constexpr auto num_fields = std::tuple_size<Fields>{};
-
-    if constexpr (num_fields <= 1) {
-      return true;
-    } else {
-      if constexpr (_i == num_fields) {
-        return true;
-      } else if constexpr (_j == -1) {
-        return no_duplicate_field_names<_i + 1, _i>();
-      } else {
-        using FieldType1 =
-            std::decay_t<typename std::tuple_element<_i, Fields>::type>;
-        using FieldType2 =
-            std::decay_t<typename std::tuple_element<_j, Fields>::type>;
-
-        constexpr auto field_name_i = FieldType1::name_;
-        constexpr auto field_name_j = FieldType2::name_;
-
-        constexpr bool no_duplicate = (field_name_i != field_name_j);
-
-        static_assert(no_duplicate, "Duplicate field names are not allowed");
-
-        return no_duplicate && no_duplicate_field_names<_i, _j - 1>();
-      }
-    }
+    return internal::no_duplicate_field_names<Fields>();
   }
 
   /// Replaced the field signified by the field type.
