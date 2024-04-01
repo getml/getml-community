86e3f210f056fd85f8d40b7a3af2ddd4e464ab1a
Patrick Urbanke <patrick@getml.com>
2024-04-01 17:59:55 +0200

    Use manylinux_2_28

--- build_scripts/build_wheel.sh
+++ build_scripts/build_wheel.sh
@@ -1,4 +1,4 @@
-# Builds the python wheel package
+# Bids the python wheel package
 function build_wheel() {
     echo -e "\n * ${COLOR_GREEN}Building the wheel package...${COLOR_RESET}\n"
     WHEEL_FILE_NAME=getml-$VERSION_NUMBER-py3-none-manylinux_2_17_$WHEEL_ARCH.whl
@@ -12,13 +12,13 @@ function build_wheel() {
     # Necessary workaround because of a bug in auditwheel
     # mv getml-$VERSION_NUMBER-py3-none-manylinux_2_17_$WHEEL_ARCH.whl getml-$VERSION_NUMBER-py38-none-manylinux_2_17_$WHEEL_ARCH.whl || exit 1
 
-    rm -rf wheelhouse || exit 1
-    auditwheel addtag getml-$VERSION_NUMBER-py3-none-manylinux_2_17_$WHEEL_ARCH.whl || exit 1
+    #rm -rf wheelhouse || exit 1
+    #auditwheel addtag getml-$VERSION_NUMBER-py3-none-manylinux_2_17_$WHEEL_ARCH.whl || exit 1
 
     # Necessary workaround because of a bug in auditwheel
-    cd wheelhouse || exit
+    #cd wheelhouse || exit
     # mv getml-$VERSION_NUMBER-py37-none-manylinux2014_$WHEEL_ARCH.linux_$WHEEL_ARCH.whl getml-$VERSION_NUMBER-py37-none-manylinux2014_$WHEEL_ARCH.whl || exit 1
-    cd $HOMEDIR/../src/python-api || exit 1
+    #cd $HOMEDIR/../src/python-api || exit 1
 
-    cp wheelhouse/getml-*.whl $GETML_BUILD_FOLDER || exit 1
+    cp getml-*.whl $GETML_BUILD_FOLDER || exit 1
 }
