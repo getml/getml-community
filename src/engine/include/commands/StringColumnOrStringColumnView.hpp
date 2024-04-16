e92ce8e5e1dd1eb130e70347f2c0d947e09b1bd2
Patrick Urbanke <patrick@getml.com>
2024-04-16 14:08:41 +0200

    Used the using Tag syntax where appropriate

--- src/engine/include/commands/StringColumnOrStringColumnView.hpp
+++ src/engine/include/commands/StringColumnOrStringColumnView.hpp
@@ -26,9 +26,8 @@ class StringColumnOrStringColumnView {
  public:
   /// The command used to concat two strings.
   struct StringBinaryOp {
-    using StringBinaryOpLiteral = rfl::Literal<"concat">;
+    using Tag = rfl::Literal<"concat">;
 
-    rfl::Field<"operator_", StringBinaryOpLiteral> op;
     rfl::Field<"operand1_", rfl::Ref<StringColumnOrStringColumnView>> operand1;
     rfl::Field<"operand2_", rfl::Ref<StringColumnOrStringColumnView>> operand2;
     rfl::Field<"type_", rfl::Literal<"StringColumnView">> type;
@@ -36,14 +35,14 @@ class StringColumnOrStringColumnView {
 
   /// The command used for string const operations.
   struct StringConstOp {
-    rfl::Field<"operator_", rfl::Literal<"const">> op;
+    using Tag = rfl::Literal<"const">;
     rfl::Field<"value_", std::string> value;
     rfl::Field<"type_", rfl::Literal<"StringColumnView">> type;
   };
 
   /// The command used to retrieve a substring.
   struct StringSubstringOp {
-    rfl::Field<"operator_", rfl::Literal<"substr">> op;
+    using Tag = rfl::Literal<"substr">;
     rfl::Field<"begin_", size_t> begin;
     rfl::Field<"len_", size_t> len;
     rfl::Field<"operand1_", rfl::Ref<StringColumnOrStringColumnView>> operand1;
@@ -52,7 +51,7 @@ class StringColumnOrStringColumnView {
 
   /// The command used for string unary operations.
   struct StringUnaryOp {
-    rfl::Field<"operator_", rfl::Literal<"as_str">> op;
+    using Tag = rfl::Literal<"as_str">;
     rfl::Field<"operand1_", std::variant<rfl::Ref<FloatColumnOrFloatColumnView>,
                                          rfl::Ref<BooleanColumnView>>>
         operand1;
@@ -61,7 +60,7 @@ class StringColumnOrStringColumnView {
 
   /// The command used for string with subtoles operations.
   struct StringWithSubrolesOp {
-    rfl::Field<"operator_", rfl::Literal<"str_with_subroles">> op;
+    using Tag = rfl::Literal<"str_with_subroles">;
     rfl::Field<"subroles_", std::vector<std::string>> subroles;
     rfl::Field<"operand1_", rfl::Ref<StringColumnOrStringColumnView>> operand1;
     rfl::Field<"type_", rfl::Literal<"StringColumnView">> type;
@@ -69,7 +68,7 @@ class StringColumnOrStringColumnView {
 
   /// The command used for string with unit operations.
   struct StringWithUnitOp {
-    rfl::Field<"operator_", rfl::Literal<"str_with_unit">> op;
+    using Tag = rfl::Literal<"str_with_unit">;
     rfl::Field<"unit_", std::string> unit;
     rfl::Field<"operand1_", rfl::Ref<StringColumnOrStringColumnView>> operand1;
     rfl::Field<"type_", rfl::Literal<"StringColumnView">> type;
@@ -77,7 +76,7 @@ class StringColumnOrStringColumnView {
 
   /// The command used for boolean subselection operations.
   struct StringSubselectionOp {
-    rfl::Field<"operator_", rfl::Literal<"str_subselection">> op;
+    using Tag = rfl::Literal<"str_subselection">;
     rfl::Field<"operand1_", rfl::Ref<StringColumnOrStringColumnView>> operand1;
     rfl::Field<"operand2_",
                std::variant<rfl::Ref<BooleanColumnView>,
@@ -88,7 +87,7 @@ class StringColumnOrStringColumnView {
 
   /// The command used to update a string column.
   struct StringUpdateOp {
-    rfl::Field<"operator_", rfl::Literal<"str_update">> op;
+    using Tag = rfl::Literal<"str_update">;
     rfl::Field<"operand1_", rfl::Ref<StringColumnOrStringColumnView>> operand1;
     rfl::Field<"operand2_", rfl::Ref<StringColumnOrStringColumnView>> operand2;
     rfl::Field<"condition_", rfl::Ref<BooleanColumnView>> condition;
@@ -97,7 +96,7 @@ class StringColumnOrStringColumnView {
 
   /// The command used for retrieving string columns from a data frame.
   struct StringColumnOp {
-    rfl::Field<"operator_", rfl::Literal<"StringColumn">> op;  // ADDED
+    using Tag = rfl::Literal<"StringColumn">;
     rfl::Field<"df_name_", std::string> df_name;
     rfl::Field<"name_", std::string> name;
     rfl::Field<"type_", rfl::Literal<"StringColumn">> type;
