40b26715da7f952e2aa701d5e5b5660eb33d9180
Patrick Urbanke <patrick@getml.com>
2024-04-20 11:50:38 +0200

    Compile arrow using Ninja

--- build_scripts/compile_arrow.sh
+++ build_scripts/compile_arrow.sh
@@ -1,8 +1,20 @@
 function compile_arrow() {
+    set -e
     cd $DEPENDENCY_DIR/arrow/cpp || exit 1
-    mkdir -p release || exit 1
-    cd release || exit 1
-    cmake -DARROW_CSV=ON -DARROW_PARQUET=ON -DARROW_WITH_BZ2=ON -DARROW_WITH_ZLIB=ON -DARROW_WITH_LZ4=ON -DARROW_WITH_SNAPPY=ON -DARROW_WITH_ZSTD=ON -DARROW_WITH_BROTLI=ON .. || exit 1
+
+    cmake \
+        -DARROW_CSV=ON \
+        -DARROW_PARQUET=ON \
+        -DARROW_WITH_BZ2=ON \
+        -DARROW_WITH_ZLIB=ON \
+        -DARROW_WITH_LZ4=ON \
+        -DARROW_WITH_SNAPPY=ON \
+        -DARROW_WITH_ZSTD=ON \
+        -DARROW_WITH_BROTLI=ON \
+        -G Ninja \
+        -B release \
+        -S . || exit 1
+
     export NUM_THREADS=$(nproc --all)
-    make -j$NUM_THREADS || exit 1
+    cmake --build release -j$NUM_THREADS || exit 1
 }
