7db63fd1c24f89370ee27cad41bcd0e453208288
Patrick Urbanke <patrick@getml.com>
2024-04-24 19:55:14 +0200

    Use reflect-cpp v0.9.0

--- build_scripts/checkout_reflectcpp.sh
+++ build_scripts/checkout_reflectcpp.sh
@@ -2,5 +2,5 @@ function checkout_reflectcpp() {
     cd $DEPENDENCY_DIR || exit 1
     git clone https://github.com/getml/reflect-cpp.git 
     cd reflect-cpp || exit 1
-    git checkout 648d628de57211d2eda8fba07140b1d35503cfde || exit 1
+    git checkout v0.9.0 || exit 1
 }
