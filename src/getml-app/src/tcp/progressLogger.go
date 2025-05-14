3aa929ba1884017868fdd0454b8c47127c57b2ce
Sören Nikolaus <soeren@getml.com>
2025-05-14 17:38:04 +0200

    Update copyright year

--- src/getml-app/src/tcp/progressLogger.go
+++ src/getml-app/src/tcp/progressLogger.go
@@ -1,4 +1,4 @@
-// Copyright 2024 Code17 GmbH
+// Copyright 2025 Code17 GmbH
 // 
 // This file is licensed under the Elastic License 2.0 (ELv2). 
 // Refer to the LICENSE.txt file in the root of the repository 
